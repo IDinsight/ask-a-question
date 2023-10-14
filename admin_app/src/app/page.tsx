202c6c463d91a5dcae2fb809ad9edfff141bb29e
Sid Ravinutala <sid.ravi1@gmail.com>
2023-10-14 04:37:17 -0400

    added grid based card layout

---
+++ admin_app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import CardPage from "../components/CardPage";
+
+export default function Home() {
+  return (
+    <main className="flex-wrap items-center justify-between ">
+      <CardPage />
+    </main>
+  );
+}
