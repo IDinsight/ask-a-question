fc66bf43698f7f377aa4753d8956b92c6a506721
ziakhan04 <zia.khan@idinsight.org>
2023-10-17 18:31:44 +0530

    AAQ-105 rename and refactor db and vectordb

--- core_backend/app/db/db_models.py
+++ core_backend/app/db/db_models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from typing import Dict, List
 
-from sqlalchemy import JSON, DateTime, ForeignKey, Integer, String
+from sqlalchemy import JSON, DateTime, ForeignKey, Integer, String, select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+from ..schemas import FeedbackBase, UserQueryBase, UserQueryResponse
 
 JSONDict = Dict[str, str]
 
@@ -13,7 +16,7 @@ class Base(DeclarativeBase):
     pass
 
 
-class UserQuery(Base):
+class UserQueryDB(Base):
     """
     SQLAlchemy data model for questions asked by user
     """
@@ -28,8 +31,8 @@ class UserQuery(Base):
     query_metadata: Mapped[JSONDict] = mapped_column(JSON, nullable=False)
     query_datetime_utc: Mapped[datetime] = mapped_column(DateTime, nullable=False)
 
-    feedback: Mapped[List["Feedback"]] = relationship(
-        "Feedback", back_populates="query", lazy=True
+    feedback: Mapped[List["FeedbackDB"]] = relationship(
+        "FeedbackDB", back_populates="query", lazy=True
     )
     responses: Mapped[List["UserQueryResponsesDB"]] = relationship(
         "UserQueryResponsesDB", back_populates="query", lazy=True
@@ -38,6 +41,40 @@ class UserQuery(Base):
     def __repr__(self) -> str:
         """Pretty Print"""
         return f"<Query #{self.query_id}> {self.query_text}>"
+
+
+async def save_user_query_to_db(
+    asession: AsyncSession, feedback_secret_key: str, user_query: UserQueryBase
+) -> UserQueryDB:
+    """
+    Saves a user query to the database.
+    """
+    user_query_db = UserQueryDB(
+        feedback_secret_key=feedback_secret_key,
+        query_datetime_utc=datetime.utcnow(),
+        **user_query.model_dump(),
+    )
+    asession.add(user_query_db)
+    await asession.commit()
+    await asession.refresh(user_query_db)
+    return user_query_db
+
+
+async def check_secret_key_match(
+    secret_key: str, query_id: int, asession: AsyncSession
+) -> bool:
+    """
+    Check if the secret key matches the one generated for query id
+    """
+    stmt = select(UserQueryDB.feedback_secret_key).where(
+        UserQueryDB.query_id == query_id
+    )
+    query_record = (await asession.execute(stmt)).first()
+
+    if (query_record is not None) and (query_record[0] == secret_key):
+        return True
+    else:
+        return False
 
 
 class UserQueryResponsesDB(Base):
@@ -52,8 +89,8 @@ class UserQueryResponsesDB(Base):
     responses: Mapped[JSONDict] = mapped_column(JSON, nullable=False)
     response_datetime_utc: Mapped[datetime] = mapped_column(DateTime, nullable=False)
 
-    query: Mapped[UserQuery] = relationship(
-        "UserQuery", back_populates="responses", lazy=True
+    query: Mapped[UserQueryDB] = relationship(
+        "UserQueryDB", back_populates="responses", lazy=True
     )
 
     def __repr__(self) -> str:
@@ -61,7 +98,24 @@ class UserQueryResponsesDB(Base):
         return f"<Responses for query #{self.query_id}"
 
 
-class Feedback(Base):
+async def save_query_response_to_db(
+    asession: AsyncSession, user_query_db: UserQueryDB, responses: UserQueryResponse
+) -> UserQueryResponsesDB:
+    """
+    Saves the user query response to the database.
+    """
+    user_query_responses_db = UserQueryResponsesDB(
+        query_id=user_query_db.query_id,
+        responses=responses.model_dump()["responses"],
+        response_datetime_utc=datetime.utcnow(),
+    )
+    asession.add(user_query_responses_db)
+    await asession.commit()
+    await asession.refresh(user_query_responses_db)
+    return user_query_responses_db
+
+
+class FeedbackDB(Base):
     """
     SQLAlchemy data model for feedback provided by user
     """
@@ -75,8 +129,8 @@ class Feedback(Base):
     feedback_text: Mapped[str] = mapped_column(String, nullable=False)
     feedback_datetime_utc: Mapped[datetime] = mapped_column(DateTime, nullable=False)
 
-    query: Mapped[UserQuery] = relationship(
-        "UserQuery", back_populates="feedback", lazy=True
+    query: Mapped[UserQueryDB] = relationship(
+        "UserQueryDB", back_populates="feedback", lazy=True
     )
 
     def __repr__(self) -> str:
@@ -85,3 +139,20 @@ class Feedback(Base):
             f"<Feedback #{self.feedback_id} for query "
             f"#{self.query_id}> {self.feedback_text}"
         )
+
+
+async def save_feedback_to_db(
+    asession: AsyncSession, feedback: FeedbackBase
+) -> FeedbackDB:
+    """
+    Saves feedback to the database.
+    """
+    feedback_db = FeedbackDB(
+        feedback_datetime_utc=datetime.utcnow(),
+        query_id=feedback.query_id,
+        feedback_text=feedback.feedback_text,
+    )
+    asession.add(feedback_db)
+    await asession.commit()
+    await asession.refresh(feedback_db)
+    return feedback_db
