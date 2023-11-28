bc92bb31a56d8f7eebeb659aa8ba3b88e63e65f3
ziakhan04 <zia.khan@idinsight.org>
2023-11-28 22:08:58 +0530

    Add clicking away from edit card removes card

--- admin_app/src/app/page.tsx
+++ admin_app/src/app/page.tsx
@@ -178,8 +178,14 @@ export default function Home() {
 
         {showEditModal ? (
           <>
-            <div className="flex backdrop-blur justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div
+              className="flex backdrop-blur justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+              onClick={() => setShowEditModal(false)}
+            >
+              <div
+                className="relative w-auto my-6 mx-auto max-w-3xl"
+                onClick={(e) => e.stopPropagation()}
+              >
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full dark:bg-gray-700 bg-gray-200 dark:outline-none focus:outline-none">
                   <div className="flex items-start justify-between p-5 rounded-t ">
                     <h3 className="text-xl">
