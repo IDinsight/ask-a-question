264ca274ccb2ee7cd9ef7045aad87c9f5994cab2
Sid Ravinutala <sid.ravi1@gmail.com>
2023-12-06 17:22:49 -0500

    [AAQ-144] Added prometheus middleware (#50)
    * Added prometheus middleware
    * added env var to docs
    * added PROMETHEUS_MULTIPROC_DIR to test.env

---
+++ core_backend/app/prometheus_middleware.py
@@ -0,0 +1,49 @@
+from typing import Callable
+
+from fastapi import FastAPI
+from prometheus_client import Counter, Histogram
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
+
+
+class PrometheusMiddleware(BaseHTTPMiddleware):
+    """
+    Prometheus middleware for FastAPI.
+    """
+
+    def __init__(self, app: FastAPI) -> None:
+        """
+        This middleware will collect metrics about requests made to the application
+        and expose them on the `/metrics` endpoint.
+        """
+        super().__init__(app)
+        self.counter = Counter(
+            "requests",
+            "Number of requests served, by http code",
+            ["method", "code", "endpoint"],
+        )
+        self.histogram = Histogram(
+            "durations_seconds",
+            "Request duration in seconds",
+            ["method", "endpoint"],
+        )
+
+    async def dispatch(self, request: Request, call_next: Callable) -> Response:
+        """
+        Collect metrics about requests made to the application.
+        """
+
+        if request.url.path == "/metrics":
+            return await call_next(request)
+
+        with self.histogram.labels(
+            method=request.method, endpoint=request.url.path
+        ).time():
+            response = await call_next(request)
+
+        self.counter.labels(
+            method=request.method, code=response.status_code, endpoint=request.url.path
+        ).inc()
+
+        return response
