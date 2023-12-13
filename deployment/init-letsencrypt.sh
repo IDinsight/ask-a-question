890c08e7835eff56c1017e5f28084dade4f75206
amir_emami <amirali1376@gmail.com>
2023-12-13 13:51:22 +0000

    Fix "certificate exists" statement in init-letsencrypt.sh

--- deployment/init-letsencrypt.sh
+++ deployment/init-letsencrypt.sh
@@ -20,7 +20,7 @@ data_path="./data/certbot"
 email="$MAIL" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
+if [ -d "$data_path/conf/live/$domains" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
