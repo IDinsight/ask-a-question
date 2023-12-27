a875e1e32aac88480d2ac153ae6cf0e9bef27828
Sid Ravinutala <sid.ravi1@gmail.com>
2023-12-27 14:36:04 -0500

    Added more test data

--- core_backend/tests/rails/test_llm_response_in_context.py
+++ core_backend/tests/rails/test_llm_response_in_context.py
@@ -3,9 +3,23 @@ These tests check LLM response content validation functions.
 LLM response content validation functions are rails that check if the responses
 are based on the given context or not.
 """
+from typing import Dict, List, Literal
+
 import pytest
+import yaml
 
 pytestmark = pytest.mark.rails
+
+CONTEXT_RESPONSE_FILE = "tests/rails/data/llm_response_in_context.yaml"
+TestDataKeys = Literal["context", "statement", "expected", "reason"]
+
+
+def read_test_data(file: str) -> List[Dict[TestDataKeys, str]]:
+    """Reads test data from file and returns a list of strings"""
+
+    with open(file, "r") as f:
+        content = yaml.safe_load(f)
+        return [dict(context=c["context"], **t) for c in content for t in c["tests"]]
 
 
 def test_alignScore(llm_sentence: str, context: str, expected_answer: bool) -> None:
