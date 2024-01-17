72930e8bf5ce96d2d3288f71c1d965ad25b56f86
wambui gitau <ywambui10@gmail.com>
2024-01-17 16:45:52 +0300

    test frontend round 1

--- infrastructure/demo/main/gh.tf
+++ infrastructure/demo/main/gh.tf
@@ -30,3 +30,59 @@ resource "aws_iam_role" "github_actions" {
   name               = "github-actions"
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
+
+
+data "aws_iam_policy_document" "gh_actions_policy_document_frontend" {
+  statement {
+    actions = ["sts:AssumeRole", "iam:GetRole", "iam:PassRole"]
+    # Set the web-task-role as the resource
+    resources = [aws_iam_role.web_task_role.arn]
+  }
+
+
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:GetAuthorizationToken",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+    "ecr:PutImage"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ecs:DescribeTaskDefinition",
+    "ecs:RegisterTaskDefinition"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ecs:DescribeServices",
+    "ecs:UpdateService"]
+    resources = [aws_ecs_service.admin_app_service.id, aws_ecs_service.backend_service.id, aws_ecs_service.nginx_service.id]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["logs:CreateLogStream"]
+    resources = ["*"]
+  }
+
+}
+
+
+
+resource "aws_iam_policy" "github_actions_resource_policies_frontend" {
+  name        = "github-actions-role-policy"
+  description = "Grant Github Actions access to assume the web-task-role"
+  policy      = data.aws_iam_policy_document.gh_actions_policy_document_frontend.json
+}
