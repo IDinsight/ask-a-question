f2dfd3ec6e8d7c663a86921a23b043141d6b1799
Sid Ravinutala <sid.ravi1@gmail.com>
2024-01-22 21:46:44 -0500

    single migration file

--- core_backend/migrations/versions/9eaf63e424ea_create_error_table.py
+++ core_backend/migrations/versions/9eaf63e424ea_create_error_table.py
@@ -1,8 +1,8 @@
-"""Error table added
+"""Create error table
 
-Revision ID: 32a59499d174
+Revision ID: 9eaf63e424ea
 Revises: 56b62dbe2d89
-Create Date: 2024-01-19 14:39:56.800794
+Create Date: 2024-01-22 21:45:29.559047
 
 """
 from typing import Sequence, Union
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "32a59499d174"
+revision: str = "9eaf63e424ea"
 down_revision: Union[str, None] = "56b62dbe2d89"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
