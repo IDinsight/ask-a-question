fd3c0a3cde1be91cf7b726656363a602e57d10aa
Carlos Samey <carlossamey@gmail.com>
2024-02-27 15:35:29 +0300

    Fix validation script

--- core_backend/validation/conftest.py
+++ core_backend/validation/conftest.py
@@ -1,17 +1,8 @@
-from datetime import datetime
-
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from core_backend.app import create_app
-from core_backend.app.db.db_models import (
-    UserQueryDB,
-    UserQueryResponseDB,
-    UserQueryResponseErrorDB,
-)
 from core_backend.app.db.engine import get_session
-from core_backend.app.schemas import UserQueryResponse, UserQueryResponseError
 
 
 @pytest.fixture(scope="session")
@@ -32,64 +23,6 @@ def db_session() -> pytest.FixtureRequest:
     finally:
         session.rollback()
         next(session_gen, None)
-
-
-@pytest.fixture(scope="session")
-def monkeysession() -> pytest.FixtureRequest:
-    from _pytest.monkeypatch import MonkeyPatch
-
-    mpatch = MonkeyPatch()
-    yield mpatch
-    mpatch.undo()
-
-
-@pytest.fixture(scope="session", autouse=True)
-def patch_save_to_db(monkeysession: pytest.FixtureRequest) -> None:
-    async def mock_save_query_to_db(
-        asession: AsyncSession, feedback_secret_key: str, user_query: UserQueryDB
-    ) -> UserQueryDB:
-        return UserQueryDB(
-            query_id=1,
-            feedback_secret_key=feedback_secret_key,
-            query_datetime_utc=datetime.utcnow(),
-            **user_query.model_dump(),
-        )
-
-    async def mock_save_response_to_db(
-        asession: AsyncSession, user_query_db: UserQueryDB, response: UserQueryResponse
-    ) -> UserQueryResponseDB:
-        return UserQueryResponseDB(
-            query_id=user_query_db.query_id,
-            content_response=response.model_dump()["content_response"],
-            llm_response=response.model_dump()["llm_response"],
-            response_datetime_utc=datetime.utcnow(),
-        )
-
-    async def mock_save_error_to_db(
-        asession: AsyncSession,
-        user_query_db: UserQueryDB,
-        error: UserQueryResponseError,
-    ) -> UserQueryResponseErrorDB:
-        return UserQueryResponseErrorDB(
-            query_id=user_query_db.query_id,
-            error_message=error.model_dump()["error_message"],
-            error_type=error.model_dump()["error_type"],
-            error_datetime_utc=datetime.utcnow(),
-            debug_info=error.model_dump()["debug_info"],
-        )
-
-    monkeysession.setattr(
-        "core_backend.app.routers.question_answer.save_user_query_to_db",
-        mock_save_query_to_db,
-    )
-    monkeysession.setattr(
-        "core_backend.app.routers.question_answer.save_query_response_to_db",
-        mock_save_response_to_db,
-    )
-    monkeysession.setattr(
-        "core_backend.app.routers.question_answer.save_query_response_error_to_db",
-        mock_save_error_to_db,
-    )
 
 
 # create command line arguments for pytest as per https://stackoverflow.com/a/42145604/7664921
