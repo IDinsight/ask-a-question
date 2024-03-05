a35c21803fe9dc4410d61962c58dae665410ed79
Wambui Gitau <ywambui10@gmail.com>
2024-03-05 11:57:47 +0300

    Aaq 279 create nginx pipeline (#92)
    * code working without nginx
    
    * working pending cleaning code
    
    * working pending cleaning code
    
    * working infrastructure and code
    
    * added information about attaching IP to domain
    
    * changes to gitignore
    
    * changes to gitignore
    
    * changes to gitignore
    
    * changes to gitignore
    
    * added information on updating service in Readme
    
    * set staging to 0
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * test frontend round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * testing pipeline round 1
    
    * Test Backend Docker Compose
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Backend Round 1
    
    * Test Nginx Round 1
    
    * Test Nginx Round 1
    
    * Test Nginx Round 1
    
    * Test Nginx Round 1
    
    * working
    
    * Test Round 2
    
    * checkpoint
    
    * Final Tests
    
    * testing cert location
    
    * Fixed cert issue
    
    * Fixed cert issue
    
    * Fixed date issue
    
    * Fixed date format issue
    
    * Fixed date format issue
    
    * Fixed date format issue
    
    * Fixed date format issue
    
    * Moved env to github
    
    * Moved env to github
    
    * uncommented tests workflow
    
    * Changing namings
    
    * Fix Merge Conflict
    
    * Fix Merge Conflict
    
    * fix env var names and format clean up
    
    * upgrade actions
    
    ---------
    
    Co-authored-by: Suzin You <suzinyou.sy@gmail.com>

--- nginx/certbot_entrypoint.sh
+++ nginx/certbot_entrypoint.sh
@@ -59,30 +59,30 @@ NGINX_CONF="/etc/nginx/conf.d/app.conf"
 
 # Check for existing certificates for the domain
 while true; do
-if [[ -d "$FULLCERT_PATH" ]]; then
+if [[ -f "$FULLCERT_PATH" ]]; then
     echo "Certificates already exist for $DOMAIN, checking if they need to be renewed..."
     start_date=$(openssl x509 -startdate -noout -in "$CERT_PATH" | cut -d= -f2)
-    start_date_epoch=$(date -d "$start_date" +%s)
+    start_date_epoch=$(date -u -D "%b %e %H:%M:%S %Y" -d "$start_date" +%s)
     current_epoch=$(date +%s)
 
     # Calculate the difference in seconds
     diff_epoch=$((current_epoch - start_date_epoch))
 
-    # Check if 12 hours (43200 seconds) have passed
-    if [ $diff_epoch -ge 43200 ]; then
-    echo "It's been more than 12 hours since the cert was created."
+    # Check if 1 week (604800 seconds) have passed
+    if [ $diff_epoch -ge 604800 ]; then
+    echo "It's been more than 1 week since the cert was created."
     echo "Renewing the certificate..."
     certbot renew
     
     else
-    hours_left=$(( (43200 - diff_epoch) / 3600 ))
+    hours_left=$(( (604800 - diff_epoch) / 3600 ))
     seconds_left=$(( hours_left * 3600 ))
-    echo "The cert was created less than 12 hours ago. Wait for $hours_left hours."
+    echo "The cert was created less than 1 week ago. Wait for $hours_left hours."
     fi
 else
     echo "No certificates found for $DOMAIN, obtaining one..."
     obtain_certificate
-    seconds_left=43200
+    seconds_left=604800
     
     if [[ -d "$CERT_PATH_FOLDER" ]]; then
         echo "Certificates obtained successfully."
