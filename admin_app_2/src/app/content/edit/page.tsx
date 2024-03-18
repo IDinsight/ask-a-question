6b2a3a16fa684ecd19b695397a2766706c22c878
Sid Ravinutala <sid.ravi1@gmail.com>
2024-03-17 22:13:27 -0400

    MUI content pages (#130)
    Basic content grid
    Alerts and Snackbars
    Fixed warnings and errors in console
    Better modals
    Better validation errors

---
+++ admin_app_2/src/app/content/edit/page.tsx
@@ -0,0 +1,259 @@
+"use client";
+import LanguageButtonBar from "@/components/LanguageButtonBar";
+import { Layout } from "@/components/Layout";
+import { appColors, appStyles, sizes } from "@/utils";
+import { apiCalls } from "@/utils/api";
+import { ChevronLeft } from "@mui/icons-material";
+import { Button, CircularProgress, TextField, Typography } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+interface Content extends EditContentBody {
+  content_id: number | null;
+  created_datetime_utc: string | null;
+  updated_datetime_utc: string | null;
+}
+
+interface EditContentBody {
+  content_title: string;
+  content_text: string;
+  content_language: string;
+  content_metadata: Record<string, unknown>;
+}
+
+const AddEditContentPage = () => {
+  const searchParams = useSearchParams();
+  const content_id = Number(searchParams.get("content_id")) || null;
+
+  const [content, setContent] = React.useState<Content | null>(null);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    if (!content_id) {
+      setIsLoading(false);
+      return;
+    } else {
+      apiCalls.getContent(content_id).then((data) => {
+        setContent(data);
+        setIsLoading(false);
+      });
+    }
+  }, [content_id]);
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100%",
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
+  return (
+    <Layout.FlexBox flexDirection={"column"} sx={{ p: sizes.doubleBaseGap }}>
+      <Header content_id={content_id} />
+      <Layout.FlexBox
+        flexDirection={"column"}
+        sx={{ px: sizes.doubleBaseGap, mx: sizes.smallGap }}
+      >
+        <Layout.Spacer multiplier={2} />
+        <ContentBox content={content} setContent={setContent} />
+        <Layout.Spacer multiplier={1} />
+      </Layout.FlexBox>
+    </Layout.FlexBox>
+  );
+};
+
+const ContentBox = ({
+  content,
+  setContent,
+}: {
+  content: Content | null;
+  setContent: React.Dispatch<React.SetStateAction<Content | null>>;
+}) => {
+  const [isSaved, setIsSaved] = React.useState(true);
+  const [saveError, setSaveError] = React.useState(false);
+  const [isTitleEmpty, setIsTitleEmpty] = React.useState(false);
+  const [isContentEmpty, setIsContentEmpty] = React.useState(false);
+
+  const router = useRouter();
+  const saveContent = async (content: Content) => {
+    const body: EditContentBody = {
+      content_title: content.content_title,
+      content_text: content.content_text,
+      content_language: content.content_language,
+      content_metadata: content.content_metadata,
+    };
+
+    const promise =
+      content.content_id === null
+        ? apiCalls.addContent(body)
+        : apiCalls.editContent(content.content_id, body);
+
+    const result = promise
+      .then((data) => {
+        setIsSaved(true);
+        setSaveError(false);
+        return data.content_id;
+      })
+      .catch((error: Error) => {
+        console.error("Error processing content:", error);
+        setSaveError(true);
+        return null;
+      });
+
+    return await result;
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    key: keyof Content,
+  ) => {
+    const emptyContent: Content = {
+      content_id: null,
+      created_datetime_utc: null,
+      updated_datetime_utc: null,
+      content_title: "",
+      content_text: "",
+      content_language: "ENGLISH",
+      content_metadata: {},
+    };
+
+    setIsTitleEmpty(false);
+    setIsContentEmpty(false);
+
+    content
+      ? setContent({ ...content, [key]: e.target.value })
+      : setContent({ ...emptyContent, [key]: e.target.value });
+    setIsSaved(false);
+  };
+
+  return (
+    <Layout.FlexBox
+      flexDirection={"column"}
+      sx={{
+        maxWidth: "800px",
+        minWidth: "300px",
+        border: 1,
+        borderColor: appColors.darkGrey,
+        backgroundColor: appColors.lightGrey,
+        borderRadius: 4,
+        p: sizes.baseGap,
+      }}
+    >
+      <LanguageButtonBar expandable={true} />
+      <Layout.Spacer multiplier={1} />
+      <TextField
+        required
+        error={isTitleEmpty}
+        helperText={isTitleEmpty ? "Should not be empty." : " "}
+        variant="outlined"
+        label="Title"
+        sx={{
+          backgroundColor: appColors.white,
+          "& .MuiFormHelperText-root": {
+            backgroundColor: appColors.lightGrey,
+            mx: 0,
+            my: 0, // Set your desired background color here
+          },
+        }}
+        value={content ? content.content_title : ""}
+        onChange={(e) => handleChange(e, "content_title")}
+      />
+      <Layout.Spacer multiplier={1} />
+      <TextField
+        multiline
+        error={isContentEmpty}
+        helperText={isContentEmpty ? "Should not be empty." : " "}
+        required
+        variant="outlined"
+        sx={{
+          backgroundColor: appColors.white,
+          "& .MuiFormHelperText-root": {
+            backgroundColor: appColors.lightGrey,
+            mx: 0,
+            my: 0, // Set your desired background color here
+          },
+        }}
+        label="Content"
+        rows={15}
+        inputProps={{ maxLength: 2000 }}
+        value={content ? content.content_text : ""}
+        onChange={(e) => handleChange(e, "content_text")}
+      />
+      <Layout.Spacer multiplier={1} />
+      <Layout.FlexBox
+        flexDirection="row"
+        sx={{ justifyContent: "space-between" }}
+      >
+        <Button
+          variant="contained"
+          disabled={isSaved}
+          color="primary"
+          sx={[{ width: "5%" }]}
+          onClick={() => {
+            if (!content) {
+              setIsTitleEmpty(true);
+              setIsContentEmpty(true);
+            } else if (content.content_title === "") {
+              setIsTitleEmpty(true);
+            } else if (content.content_text === "") {
+              setIsContentEmpty(true);
+            } else {
+              const handleSaveContent = async (content: Content) => {
+                const content_id = await saveContent(content);
+                if (content_id) {
+                  const actionType = content.content_id ? "edit" : "add";
+                  router.push(
+                    `/content/?content_id=${content_id}&action=${actionType}`,
+                  );
+                }
+              };
+              handleSaveContent(content);
+            }
+          }}
+        >
+          Save
+        </Button>
+        {saveError ? (
+          <Alert variant="outlined" severity="error" sx={{ px: 3, py: 0 }}>
+            Failed to save content.
+          </Alert>
+        ) : null}
+      </Layout.FlexBox>
+    </Layout.FlexBox>
+  );
+};
+
+const Header = ({ content_id }: { content_id: number | null }) => {
+  return (
+    <Layout.FlexBox flexDirection="row" {...appStyles.alignItemsCenter}>
+      <ChevronLeft
+        style={{ cursor: "pointer" }}
+        onClick={() => (window.location.href = "/content/")}
+      />
+      <Layout.Spacer multiplier={1} horizontal />
+      {content_id ? (
+        <>
+          <Typography variant="h5">Edit Content</Typography>
+          <Layout.Spacer multiplier={2} horizontal />
+          <Typography variant="h5">{`\u2022`}</Typography>
+          <Layout.Spacer multiplier={2} horizontal />
+          <Typography variant="h5">#{content_id}</Typography>
+        </>
+      ) : (
+        <Typography variant="h5">Add Content</Typography>
+      )}
+    </Layout.FlexBox>
+  );
+};
+
+export default AddEditContentPage;
