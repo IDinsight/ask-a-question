9ce0aa2c6feffef2f7b1d2704bbc7a520cd9ae0a
amir_emami <amirali1376@gmail.com>
2024-04-04 18:59:37 +0530

    add to permissions

--- deployment/aws/infrastructure/testing/main/iam.tf
+++ deployment/aws/infrastructure/testing/main/iam.tf
@@ -121,7 +121,12 @@ data "aws_iam_policy_document" "web_ec2_role_policy" {
     actions = [
       "ecs:DescribeServices",
     "ecs:UpdateService"]
-    resources = [aws_ecs_service.admin_app_service.id, aws_ecs_service.backend_service.id, aws_ecs_service.nginx_service.id]
+    resources = [
+      aws_ecs_service.admin_app_service.id,
+      aws_ecs_service.backend_service.id,
+      aws_ecs_service.nginx_service.id,
+      aws_ecs_service.litellm_proxy_service.id
+    ]
   }
 
   statement {
