9e320ef54d7331977ac9e60529d84187a8acb061
amir_emami <amirali1376@gmail.com>
2024-04-25 20:06:44 +0530

    change fullaccess and readonly users to user1 and user2 with fullaccess permissions

--- core_backend/app/auth/config.py
+++ core_backend/app/auth/config.py
@@ -1,10 +1,8 @@
 import os
 
-CONTENT_READONLY_PASSWORD = os.environ.get("CONTENT_READONLY_PASSWORD", "readonly")
+CONTENT_USER1_PASSWORD = os.environ.get("CONTENT_USER1_PASSWORD", "fullaccess")
+CONTENT_USER2_PASSWORD = os.environ.get("CONTENT_USER2_PASSWORD", "fullaccess")
 ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 60 * 24 * 7)
 JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM", "HS256")
 JWT_SECRET = os.environ.get("JWT_SECRET", "jwt-secret")
 QUESTION_ANSWER_SECRET = os.environ.get("QUESTION_ANSWER_SECRET", "update-me")
-CONTENT_FULLACCESS_PASSWORD = os.environ.get(
-    "CONTENT_FULLACCESS_PASSWORD", "fullaccess"
-)
