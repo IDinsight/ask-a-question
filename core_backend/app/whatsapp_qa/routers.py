16d5a6f3c7fc7c764823768c6dcb10344896f577
amir_emami <amirali1376@gmail.com>
2024-04-27 10:40:12 +0530

    Disable WhatsApp pytests - WhatsApp endpoint is broken due to no access to auth information

--- core_backend/app/whatsapp_qa/routers.py
+++ core_backend/app/whatsapp_qa/routers.py
@@ -57,7 +57,7 @@ async def process_whatsapp_message(
                 )
 
                 content_response = await get_similar_content_async(
-                    user_id="user1",  # TEMPORARY HARDCODED USER ID
+                    user_id="HARDCODED_USER_ID",  # TEMPORARY HARDCODED USER ID
                     question=msg_body,
                     n_similar=1,
                     asession=asession,
