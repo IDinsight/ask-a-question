92543c07eace85cc71f01ceb5ebb99c5a2b99d20
Carlos Samey <carlossamey@gmail.com>
2024-04-30 09:35:51 +0300

    Add prettier

--- admin_app/src/app/dashboard/page.tsx
+++ admin_app/src/app/dashboard/page.tsx
@@ -1,150 +1,182 @@
-"use client"
+"use client";
 
-import Grid from '@mui/material/Grid'
+import Grid from "@mui/material/Grid";
 
-import EmailIcon from '@mui/icons-material/Email';
-import WeeklyOverview from '@/components/BarChart';
-import RecommendIcon from '@mui/icons-material/Recommend';
-import CardStatisticsVerticalComponent from '@/components/StatisticsCard'
-import ApexChartWrapper from '@/components/ApexCharWrapper'
-import React from 'react';
+import EmailIcon from "@mui/icons-material/Email";
+import WeeklyOverview from "@/components/BarChart";
+import RecommendIcon from "@mui/icons-material/Recommend";
+import CardStatisticsVerticalComponent from "@/components/StatisticsCard";
+import ApexChartWrapper from "@/components/ApexCharWrapper";
+import React from "react";
 import { apiCalls } from "@/utils/api";
 import { useAuth } from "@/utils/auth";
-import { CircularProgress } from '@mui/material';
-import { Layout } from '@/components/Layout';
-import { sizes } from '@/utils';
-
+import { CircularProgress } from "@mui/material";
+import { Layout } from "@/components/Layout";
+import { sizes } from "@/utils";
 
 const Dashboard = () => {
-    const [isLoading, setIsLoading] = React.useState<boolean>(true);
-    const [questionStats, setQuestionStats] = React.useState<Array<number>>([]);
-    const [upvoteStats, setUpvoteStats] = React.useState<Array<number>>([]);
-    const { token } = useAuth();
-    const [labels, setLabels] = React.useState<Array<string>>([]);
-    const [lastFetch, setLastFetch] = React.useState<number>(Date.now());
-    React.useEffect(() => {
-        const fetchQuestionStats = async () => {
-            setIsLoading(true);
-            try {
-                const stats = await apiCalls.getQuestionStats(token!);
-                setQuestionStats(stats.six_months_questions);
-                setUpvoteStats(stats.six_months_upvotes)
-            } catch (error) {
-                console.error("Failed to fetch question stats:", error);
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        fetchQuestionStats();
-        const lastSixMonths = getPastSixMonths();
-        setLabels(lastSixMonths);
-        const interval = setInterval(() => {
-            setLastFetch(Date.now());
-        }, 60000);
-        return () => clearInterval(interval);
-    }, [token, lastFetch]);
-    function calculatePercentageChange(array: number[]): string {
-
-        if (array.length < 2) {
-            return 'N/A';
-        }
-        const [latest, previous] = array.slice(-2);
-
-
-        const percentageChange = Math.abs((latest - previous) / (previous > 0 ? previous : 1));
-
-        return (percentageChange * 100).toFixed(2) + '%';
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [questionStats, setQuestionStats] = React.useState<Array<number>>([]);
+  const [upvoteStats, setUpvoteStats] = React.useState<Array<number>>([]);
+  const { token } = useAuth();
+  const [labels, setLabels] = React.useState<Array<string>>([]);
+  const [lastFetch, setLastFetch] = React.useState<number>(Date.now());
+  React.useEffect(() => {
+    const fetchQuestionStats = async () => {
+      setIsLoading(true);
+      try {
+        const stats = await apiCalls.getQuestionStats(token!);
+        setQuestionStats(stats.six_months_questions);
+        setUpvoteStats(stats.six_months_upvotes);
+      } catch (error) {
+        console.error("Failed to fetch question stats:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchQuestionStats();
+    const lastSixMonths = getPastSixMonths();
+    setLabels(lastSixMonths);
+    const interval = setInterval(() => {
+      setLastFetch(Date.now());
+    }, 60000);
+    return () => clearInterval(interval);
+  }, [token, lastFetch]);
+  function calculatePercentageChange(array: number[]): string {
+    if (array.length < 2) {
+      return "N/A";
     }
-    function getPastSixMonths(): string[] {
-        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const [latest, previous] = array.slice(-2);
 
+    const percentageChange = Math.abs(
+      (latest - previous) / (previous > 0 ? previous : 1)
+    );
 
-        const today = new Date();
+    return (percentageChange * 100).toFixed(2) + "%";
+  }
+  function getPastSixMonths(): string[] {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
 
-        let pastSixMonths = [];
+    const today = new Date();
 
+    let pastSixMonths = [];
 
-        for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
+      const monthIndex = new Date(
+        today.getFullYear(),
+        today.getMonth() - i,
+        1
+      ).getMonth();
+      const year = new Date(
+        today.getFullYear(),
+        today.getMonth() - i,
+        1
+      ).getFullYear();
 
-            const monthIndex = new Date(today.getFullYear(), today.getMonth() - i, 1).getMonth();
-            const year = new Date(today.getFullYear(), today.getMonth() - i, 1).getFullYear();
-
-            pastSixMonths.unshift(`${months[monthIndex]} ${year}`);
-        }
-
-        return pastSixMonths;
+      pastSixMonths.unshift(`${months[monthIndex]} ${year}`);
     }
-    if (isLoading) {
-        return (
-            <>
-                <Layout.FlexBox
-                    bgcolor="lightgray.main"
-                    sx={{
-                        mx: sizes.baseGap,
-                        py: sizes.tinyGap,
-                        width: "98%",
-                        minHeight: "660px",
-                    }}
-                >
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            height: "50vh",
-                            width: "100%",
-                        }}
-                    >
-                        <CircularProgress />
-                    </div>
-                </Layout.FlexBox>
 
-            </>
-        );
-    }
+    return pastSixMonths;
+  }
+  if (isLoading) {
     return (
-        <ApexChartWrapper>
-            <Grid container spacing={6} sx={{ mt: 4 }} >
-
-                <Grid item xs={12}>
-                    <Grid container justifyContent="center" spacing={6}>
-                        <Grid item xs={6} md={4} lg={4}>
-                            <CardStatisticsVerticalComponent
-                                stats={questionStats && questionStats.length > 5
-                                    ? questionStats[5].toString() : '0'}
-                                icon={<EmailIcon />}
-                                trend={questionStats && questionStats.length > 5 &&
-                                    questionStats[5] >= questionStats[4] ? "positive" : "negative"}
-                                trendNumber={questionStats ? calculatePercentageChange(questionStats) : "0"}
-                                title='Total questions'
-                            />
-                        </Grid>
-                        <Grid item xs={6} md={4} lg={4}>
-                            <CardStatisticsVerticalComponent
-                                stats={upvoteStats && upvoteStats.length > 5
-                                    ? upvoteStats[5].toString() : '0'}
-                                trend={upvoteStats && upvoteStats.length > 5 &&
-                                    upvoteStats[5] >= upvoteStats[4] ? "positive" : "negative"}
-                                trendNumber={upvoteStats ? calculatePercentageChange(upvoteStats) : "0"}
-                                title='Total upvotes'
-                                icon={<RecommendIcon />}
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={6} lg={10} sx={{ mt: 4 }}>
-                            <WeeklyOverview
-                                labels={labels ? labels : []}
-                                data={questionStats ? questionStats : []} />
-                        </Grid>
-                    </Grid>
-
-                </Grid>
-
-
+      <>
+        <Layout.FlexBox
+          bgcolor="lightgray.main"
+          sx={{
+            mx: sizes.baseGap,
+            py: sizes.tinyGap,
+            width: "98%",
+            minHeight: "660px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "50vh",
+              width: "100%",
+            }}
+          >
+            <CircularProgress />
+          </div>
+        </Layout.FlexBox>
+      </>
+    );
+  }
+  return (
+    <ApexChartWrapper>
+      <Grid container spacing={6} sx={{ mt: 4 }}>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={6}>
+            <Grid item xs={6} md={4} lg={4}>
+              <CardStatisticsVerticalComponent
+                stats={
+                  questionStats && questionStats.length > 5
+                    ? questionStats[5].toString()
+                    : "0"
+                }
+                icon={<EmailIcon />}
+                trend={
+                  questionStats &&
+                  questionStats.length > 5 &&
+                  questionStats[5] >= questionStats[4]
+                    ? "positive"
+                    : "negative"
+                }
+                trendNumber={
+                  questionStats ? calculatePercentageChange(questionStats) : "0"
+                }
+                title="Total questions"
+              />
             </Grid>
+            <Grid item xs={6} md={4} lg={4}>
+              <CardStatisticsVerticalComponent
+                stats={
+                  upvoteStats && upvoteStats.length > 5
+                    ? upvoteStats[5].toString()
+                    : "0"
+                }
+                trend={
+                  upvoteStats &&
+                  upvoteStats.length > 5 &&
+                  upvoteStats[5] >= upvoteStats[4]
+                    ? "positive"
+                    : "negative"
+                }
+                trendNumber={
+                  upvoteStats ? calculatePercentageChange(upvoteStats) : "0"
+                }
+                title="Total upvotes"
+                icon={<RecommendIcon />}
+              />
+            </Grid>
+            <Grid item xs={12} md={6} lg={10} sx={{ mt: 4 }}>
+              <WeeklyOverview
+                labels={labels ? labels : []}
+                data={questionStats ? questionStats : []}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </ApexChartWrapper>
+  );
+};
 
-        </ApexChartWrapper>
-    )
-}
-
-export default Dashboard
+export default Dashboard;
--- admin_app/src/components/BarChart.tsx
+++ admin_app/src/components/BarChart.tsx
@@ -1,103 +1,113 @@
-"use client"
-import dynamic from 'next/dynamic'
-import Card from '@mui/material/Card'
-import { useTheme } from '@mui/material/styles'
-import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
+"use client";
+import dynamic from "next/dynamic";
+import Card from "@mui/material/Card";
+import { useTheme } from "@mui/material/styles";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
 
-import { ApexOptions } from 'apexcharts'
+import { ApexOptions } from "apexcharts";
 
-const ReactApexcharts = dynamic(() => import('react-apexcharts'), { ssr: false })
+const ReactApexcharts = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 const WeeklyOverview = ({
-    labels,
-    data
+  labels,
+  data,
 }: {
-    labels: Array<string>;
-    data: Array<number>;
+  labels: Array<string>;
+  data: Array<number>;
 }) => {
+  const theme = useTheme();
 
-    const theme = useTheme()
+  const options: ApexOptions = {
+    chart: {
+      parentHeightOffset: 0,
+      toolbar: { show: false },
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 9,
+        columnWidth: "30%",
+      },
+    },
+    stroke: {
+      width: 2,
+      colors: [theme.palette.background.paper],
+    },
+    legend: { show: false },
+    grid: {
+      strokeDashArray: 7,
+      padding: {
+        top: -1,
+        right: 0,
+        left: -12,
+        bottom: 5,
+      },
+    },
+    dataLabels: { enabled: true },
+    colors: [
+      theme.palette.background.default,
+      theme.palette.background.default,
+      theme.palette.background.default,
+      theme.palette.primary.main,
+      theme.palette.background.default,
+      theme.palette.background.default,
+    ],
+    states: {
+      hover: {
+        filter: { type: "none" },
+      },
+      active: {
+        filter: { type: "none" },
+      },
+    },
+    xaxis: {
+      categories: labels,
+      tickPlacement: "on",
+      labels: { show: true },
+      axisTicks: { show: false },
+      axisBorder: { show: false },
+      title: {
+        text: "Date",
+      },
+    },
+    yaxis: {
+      show: true,
+      title: {
+        text: "Number of questions",
+      },
+      tickAmount: 5,
+      labels: {
+        formatter: (value) =>
+          `${value > 999 ? `${(value / 1000).toFixed(0)}k` : value}`,
+      },
+    },
+  };
 
-    const options: ApexOptions = {
-        chart: {
-            parentHeightOffset: 0,
-            toolbar: { show: false }
-        },
-        plotOptions: {
-            bar: {
-                borderRadius: 9,
-                columnWidth: '30%',
-            }
-        },
-        stroke: {
-            width: 2,
-            colors: [theme.palette.background.paper]
-        },
-        legend: { show: false },
-        grid: {
-            strokeDashArray: 7,
-            padding: {
-                top: -1,
-                right: 0,
-                left: -12,
-                bottom: 5
-            }
-        },
-        dataLabels: { enabled: true },
-        colors: [
-            theme.palette.background.default,
-            theme.palette.background.default,
-            theme.palette.background.default,
-            theme.palette.primary.main,
-            theme.palette.background.default,
-            theme.palette.background.default
-        ],
-        states: {
-            hover: {
-                filter: { type: 'none' }
-            },
-            active: {
-                filter: { type: 'none' }
-            }
-        },
-        xaxis: {
-            categories: labels,
-            tickPlacement: 'on',
-            labels: { show: true },
-            axisTicks: { show: false },
-            axisBorder: { show: false },
-            title: {
-                text: 'Date'
-            },
-        },
-        yaxis: {
-            show: true,
-            title: {
-                text: 'Number of questions'
-            },
-            tickAmount: 5,
-            labels: {
+  return (
+    <Card>
+      <CardHeader
+        title="Timeline"
+        titleTypographyProps={{
+          sx: {
+            lineHeight: "2rem !important",
+            letterSpacing: "0.15px !important",
+          },
+        }}
+      />
+      <CardContent
+        sx={{ "& .apexcharts-xcrosshairs.apexcharts-active": { opacity: 0 } }}
+      >
+        <ReactApexcharts
+          type="bar"
+          height={300}
+          width={"100%"}
+          options={options}
+          series={[{ name: "Monthly number of questions", data: data }]}
+        />
+      </CardContent>
+    </Card>
+  );
+};
 
-                formatter: value => `${value > 999 ? `${(value / 1000).toFixed(0)}k` : value}`
-            },
-
-        }
-    }
-
-    return (
-        <Card>
-            <CardHeader
-                title='Timeline'
-                titleTypographyProps={{
-                    sx: { lineHeight: '2rem !important', letterSpacing: '0.15px !important' }
-                }}
-            />
-            <CardContent sx={{ '& .apexcharts-xcrosshairs.apexcharts-active': { opacity: 0 } }}>
-                <ReactApexcharts type='bar' height={300} width={"100%"} options={options} series={[{ name: "Monthly number of questions", data: data }]} />
-
-            </CardContent>
-        </Card>
-    )
-}
-
-export default WeeklyOverview
+export default WeeklyOverview;
