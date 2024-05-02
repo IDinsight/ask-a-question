6fd641c246dd0d35b79ddcd44c9b8867a206f629
amir_emami <amirali1376@gmail.com>
2024-05-02 16:24:25 +0530

    Match stats card to wireframe

--- admin_app/src/components/StatisticsCard.tsx
+++ admin_app/src/components/StatisticsCard.tsx
@@ -1,13 +1,15 @@
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import Avatar from "@mui/material/Avatar";
-import TrendingUpIcon from "@mui/icons-material/TrendingUp";
-import TrendingDownIcon from "@mui/icons-material/TrendingDown";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import Typography from "@mui/material/Typography";
 import CardContent from "@mui/material/CardContent";
 import { ReactNode } from "react";
 
 import ThemeColor from "@mui/material/styles/createPalette";
+import { appColors } from "@/utils";
+import { Layout } from "./Layout";
 
 const CardStatsVertical = ({
   title,
@@ -25,73 +27,83 @@ const CardStatsVertical = ({
   trend?: "positive" | "negative";
 }) => {
   return (
-    <Card sx={{ borderRadius: "8px" }}>
+    <Card sx={{ borderRadius: "8px", minWidth: 350 }}>
       <CardContent>
         <Box
           sx={{
             display: "flex",
+            flexDirection: "column",
             alignItems: "center",
             justifyContent: "space-between",
-            marginBottom: 5.5,
+            padding: 1,
           }}
         >
           <Box
             sx={{
               display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "80%",
             }}
           >
-            <Typography sx={{ fontWeight: 600, fontSize: "1.5rem" }}>
-              {title}
-            </Typography>
-            <Box sx={{ display: "flex", alignItems: "center", mb: 4 }}>
-              <Typography variant="h4" sx={{ mr: 2 }}>
-                {stats}
-              </Typography>
-            </Box>
-            <Box sx={{ flexGrow: 1 }}></Box>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "flex-start",
+              }}
+            >
               <Typography
-                component="sup"
-                variant="caption"
-                sx={{
-                  color: trend === "positive" ? "success.main" : "error.main",
-                  mr: 0.5,
-                  fontSize: "1rem",
-                }}
+                variant="overline"
+                sx={{ fontSize: "1.2rem", color: appColors.darkGrey }}
               >
-                {trend === "positive" ? (
-                  <TrendingUpIcon sx={{ fontSize: "1rem" }} />
-                ) : (
-                  <TrendingDownIcon sx={{ fontSize: "1rem" }} />
-                )}
-                {trend === "positive" ? "+" + trendNumber : "-" + trendNumber}
+                {title}
               </Typography>
-              <Typography
-                variant="caption"
-                sx={{
-                  fontSize: "1rem",
-                }}
-              >
-                since last month
-              </Typography>
+              <Typography variant="h5">{stats}</Typography>
             </Box>
+            <Avatar
+              sx={{
+                color: "common.white",
+                backgroundColor: `${color}.main`,
+                width: 50,
+                height: 50,
+              }}
+            >
+              {icon}
+            </Avatar>
           </Box>
-
-          {/* Avatar Box */}
-          <Avatar
-            sx={{
-              boxShadow: 3,
-              color: "common.white",
-              backgroundColor: `${color}.main`,
-              width: 80,
-              height: 80,
-            }}
-          >
-            {icon}
-          </Avatar>
+          <Layout.Spacer multiplier={1} />
+          <Box sx={{ display: "flex", alignItems: "flex-start", width: "80%" }}>
+            <Typography
+              component="sup"
+              variant="caption"
+              sx={{
+                color: trend === "positive" ? "success.main" : "error.main",
+                mr: 0.5,
+                fontSize: "1rem",
+              }}
+            >
+              {trend === "positive" ? (
+                <ArrowUpwardIcon sx={{ fontSize: "1rem" }} />
+              ) : (
+                <ArrowDownwardIcon sx={{ fontSize: "1rem" }} />
+              )}
+              {" " + trendNumber}
+            </Typography>
+            <Typography
+              variant="caption"
+              sx={{
+                fontSize: "1rem",
+                color: appColors.darkGrey,
+              }}
+            >
+              since last month
+            </Typography>
+          </Box>
         </Box>
+        <Layout.Spacer multiplier={1} />
       </CardContent>
     </Card>
   );
