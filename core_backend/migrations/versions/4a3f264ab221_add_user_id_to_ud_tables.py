1d022de2f58f7848cada994c8be708689c5c350d
amir_emami <amirali1376@gmail.com>
2024-05-06 17:32:02 +0530

    fix migration version after rebase

--- core_backend/migrations/versions/4a3f264ab221_add_user_id_to_ud_tables.py
+++ core_backend/migrations/versions/4a3f264ab221_add_user_id_to_ud_tables.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "4a3f264ab221"
-down_revision: Union[str, None] = "5b7568c848c4"
+down_revision: Union[str, None] = "75235585abd7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
