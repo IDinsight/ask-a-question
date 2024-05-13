a9dd0e9d782b8f6f1e866950731db628d1598d01
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-05-13 23:03:50 +0530

    fix logo aspect ratio

--- admin_app/src/components/NavBar.tsx
+++ admin_app/src/components/NavBar.tsx
@@ -52,8 +52,8 @@ const Logo = () => {
         component="img"
         src={logowhite.src}
         sx={{
-          height: 30,
-          width: 180,
+          height: 36,
+          aspect_ratio: 1200 / 214,
           display: { xs: "none", md: "block" },
         }}
       />
