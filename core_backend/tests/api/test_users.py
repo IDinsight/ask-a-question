a917b65a906bdf1554b4d5f0caa647fae76c0a79
amir_emami <amirali1376@gmail.com>
2024-05-14 13:17:42 +0530

    Fix user tests with async session

--- core_backend/tests/api/test_users.py
+++ core_backend/tests/api/test_users.py
@@ -1,11 +1,11 @@
-from datetime import datetime
+from typing import AsyncGenerator
 
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from core_backend.app.database import get_async_session
 from core_backend.app.users.models import (
     UserAlreadyExistsError,
-    UserDB,
     UserNotFoundError,
     get_user_by_token,
     get_user_by_username,
@@ -22,60 +22,60 @@ from core_backend.tests.api.conftest import (
 )
 
 
-@pytest.mark.asyncio(scope="module")
-async def test_save_user_to_db(db_session: AsyncSession) -> None:
-    user = UserCreate(
-        user_id="2", username="test2", password="password2", retrieval_key="key2"
-    )
-    saved_user = await save_user_to_db(user, db_session)
-    assert saved_user.username == "test2"
+class TestUsers:
+    @pytest.fixture(scope="function")
+    async def asession(self) -> AsyncGenerator[AsyncSession, None]:
+        async for session in get_async_session():
+            yield session
 
+        await session.close()
 
-@pytest.mark.asyncio(scope="module")
-async def test_save_user_to_db_existing_user(db_session: AsyncSession) -> None:
-    user = UserCreate(
-        user_id=TEST_USER_ID,
-        username=TEST_USERNAME,
-        password=TEST_PASSWORD,
-        retrieval_key=TEST_USER_RETRIEVAL_KEY,
-    )
-    with pytest.raises(UserAlreadyExistsError):
-        await save_user_to_db(user, db_session)
+    async def test_save_user_to_db(self, asession: AsyncSession) -> None:
+        user = UserCreate(
+            user_id="test_user_id_2",
+            username="test_username_2",
+            password="test_password_2",
+            retrieval_key="test_retrieval_key_2",
+        )
+        saved_user = await save_user_to_db(user, asession)
+        assert saved_user.username == "test_username_2"
 
+    async def test_save_user_to_db_existing_user(self, asession: AsyncSession) -> None:
+        user = UserCreate(
+            user_id=TEST_USER_ID,
+            username=TEST_USERNAME,
+            password=TEST_PASSWORD,
+            retrieval_key=TEST_USER_RETRIEVAL_KEY,
+        )
+        with pytest.raises(UserAlreadyExistsError):
+            await save_user_to_db(user, asession)
 
-@pytest.mark.asyncio(scope="module")
-async def test_get_user_by_username(db_session: AsyncSession) -> None:
-    retrieved_user = await get_user_by_username(TEST_USERNAME, db_session)
-    assert retrieved_user.username == TEST_USERNAME
+    async def test_get_user_by_username(self, asession: AsyncSession) -> None:
+        retrieved_user = await get_user_by_username(TEST_USERNAME, asession)
+        assert retrieved_user.username == TEST_USERNAME
 
+    async def test_get_user_by_username_no_user(self, asession: AsyncSession) -> None:
+        with pytest.raises(UserNotFoundError):
+            await get_user_by_username("nonexistent", asession)
 
-@pytest.mark.asyncio(scope="module")
-async def test_get_user_by_username_no_user(db_session: AsyncSession) -> None:
-    with pytest.raises(UserNotFoundError):
-        await get_user_by_username("nonexistent", db_session)
+    async def test_get_user_by_token(self, asession: AsyncSession) -> None:
+        retrieved_user = await get_user_by_token(TEST_USER_RETRIEVAL_KEY, asession)
+        assert retrieved_user.username == TEST_USERNAME
 
+    async def test_get_user_by_token_no_user(self, asession: AsyncSession) -> None:
+        with pytest.raises(UserNotFoundError):
+            await get_user_by_token("nonexistent", asession)
 
-@pytest.mark.asyncio(scope="module")
-async def test_get_user_by_token(db_session: AsyncSession) -> None:
-    retrieved_user = await get_user_by_token(TEST_USER_RETRIEVAL_KEY, db_session)
-    assert retrieved_user.username == TEST_USERNAME
+    async def test_update_user_retrieval_key(self, asession: AsyncSession) -> None:
+        user = UserCreate(
+            user_id="test_user_id_3",
+            username="test_username_3",
+            password="test_password_3",
+            retrieval_key="test_retrieval_key_3",
+        )
+        saved_user = await save_user_to_db(user, asession)
+        assert saved_user.hashed_retrieval_key == get_key_hash("test_retrieval_key_3")
 
-
-@pytest.mark.asyncio(scope="module")
-async def test_get_user_by_token_no_user(db_session: AsyncSession) -> None:
-    with pytest.raises(UserNotFoundError):
-        await get_user_by_token("nonexistent", db_session)
-
-
-@pytest.mark.asyncio(scope="module")
-async def test_update_user_retrieval_key(db_session: AsyncSession) -> None:
-    user_db = UserDB(
-        user_id=TEST_USER_ID,
-        username=TEST_USERNAME,
-        hashed_password=get_key_hash(TEST_PASSWORD),
-        hashed_retrieval_key=get_key_hash("new_key"),
-        created_datetime_utc=datetime.utcnow(),
-        updated_datetime_utc=datetime.utcnow(),
-    )
-    updated_user = await update_user_retrieval_key(user_db, "new_key", db_session)
-    assert updated_user.hashed_retrieval_key != get_key_hash(TEST_USER_RETRIEVAL_KEY)
+        updated_user = await update_user_retrieval_key(saved_user, "new_key", asession)
+        assert updated_user.hashed_retrieval_key != get_key_hash("test_retrieval_key_3")
+        assert updated_user.hashed_retrieval_key == get_key_hash("new_key")
