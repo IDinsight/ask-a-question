f22ea2e7afb9067f870b111570d941a41a40194b
Sid Ravinutala <sid.ravi1@gmail.com>
2024-05-15 21:31:59 -0400

    fixes from PR

--- admin_app/src/app/login/page.tsx
+++ admin_app/src/app/login/page.tsx
@@ -156,19 +156,17 @@ const Login = () => {
             >
               Sign In
             </Button>
-            {NEXT_PUBLIC_GOOGLE_LOGIN_CLIENT_ID === "not-set" ? null : (
-              <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Typography variant="body1" sx={{ py: 4 }}>
-                  - or -
-                </Typography>
-                <div id="signinDiv"></div>
-              </Box>
-            )}
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Typography variant="body1" sx={{ py: 4 }}>
+                - or -
+              </Typography>
+              <div id="signinDiv"></div>
+            </Box>
           </Box>
         </Box>
       </Grid>
