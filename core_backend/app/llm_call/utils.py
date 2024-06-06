e278dce4e97bcd7b5e2895892fea5b44b5b3fe32
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-06-06 15:05:40 +0530

    move safety settings to litellm proxy config

--- core_backend/app/llm_call/utils.py
+++ core_backend/app/llm_call/utils.py
@@ -36,25 +36,6 @@ async def _ask_llm_async(
         api_base=litellm_endpoint,
         api_key=LITELLM_API_KEY,
         metadata={"generation_name": litellm_model},
-        # override default safety settings for gemini
-        safety_settings=[
-            {
-                "category": "HARM_CATEGORY_HARASSMENT",
-                "threshold": "BLOCK_NONE",
-            },
-            {
-                "category": "HARM_CATEGORY_HATE_SPEECH",
-                "threshold": "BLOCK_NONE",
-            },
-            {
-                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                "threshold": "BLOCK_NONE",
-            },
-            {
-                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                "threshold": "BLOCK_NONE",
-            },
-        ],
     )
     logger.info(f"LLM output: {llm_response_raw.choices[0].message.content}")
 
