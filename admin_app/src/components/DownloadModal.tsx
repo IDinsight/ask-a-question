ee24d0a2ec4c927cfd0dea78e0b0cd023c186c36
amir_emami <amirali1376@gmail.com>
2024-06-12 12:12:15 +0100

    fetch tag names and document pre-processing

--- admin_app/src/components/DownloadModal.tsx
+++ admin_app/src/components/DownloadModal.tsx
@@ -27,6 +27,46 @@ const DownloadModal = ({
   const { token, accessLevel } = useAuth();
   const [loading, setLoading] = useState(false);
 
+  const fetchAndCleanContents = async () => {
+    // fetch all contents
+    const raw_json_data = await apiCalls.getContentList({
+      token: token!,
+      skip: 0,
+      limit: MAX_CARDS_TO_FETCH,
+    });
+    // convert to list of json objects
+    const json_data_list = Object.values(raw_json_data);
+    // move content_id to be frst and drop user_id
+    const ordered_json_data_list = json_data_list.map((item) => {
+      const { user_id, content_id, ...rest } = item;
+      return {
+        content_id,
+        ...rest,
+      };
+    });
+    // stringify json element
+    const processed_contents_json = ordered_json_data_list.map((item) => {
+      return {
+        ...item,
+        content_metadata: JSON.stringify(item.content_metadata),
+      };
+    });
+    // get list of tags and replace tag ids with tag names
+    const tags_json = await apiCalls.getTagList(token!);
+    const tag_list = Object.values(tags_json);
+    const tag_dict = tag_list.reduce((acc, tag) => {
+      acc[tag.tag_id] = tag.tag_name;
+      return acc;
+    }, {});
+    processed_contents_json.forEach((item) => {
+      item.content_tags = item.content_tags.map(
+        (tag_id: number) => tag_dict[tag_id],
+      );
+    });
+
+    return processed_contents_json;
+  };
+
   const downloadCSV = (csvData: string, fileName: string) => {
     const blob = new Blob([csvData], { type: "text/csv" });
     const url = window.URL.createObjectURL(blob);
@@ -41,20 +81,9 @@ const DownloadModal = ({
   const handleDownloadContent = async () => {
     setLoading(true);
     try {
-      const raw_json_data = await apiCalls.getContentList({
-        token: token!,
-        skip: 0,
-        limit: MAX_CARDS_TO_FETCH,
-      });
-      const json_data_list = Object.values(raw_json_data);
-      const cleaned_json_data_list = json_data_list.map((item) => {
-        return {
-          ...item,
-          content_metadata: JSON.stringify(item.content_metadata),
-          content_tags: JSON.stringify(item.content_tags),
-        };
-      });
-      const csv = Papa.unparse(cleaned_json_data_list);
+      const processed_contents_json = await fetchAndCleanContents();
+      // convert to csv
+      const csv = Papa.unparse(processed_contents_json);
       downloadCSV(csv, "content.csv");
     } catch (error) {
       console.error("Failed to download content", error);
