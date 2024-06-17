418e431c79f63f8cc4dafa3063e731f65374f145
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-06-17 12:07:18 +0530

    remove translate and paraphrase from llm response (#247)

--- core_backend/app/question_answer/routers.py
+++ core_backend/app/question_answer/routers.py
@@ -90,9 +90,7 @@ async def llm_response(
 
 @check_align_score__after
 @identify_language__before
-@translate_question__before
 @classify_safety__before
-@paraphrase_question__before
 async def get_llm_answer(
     question: QueryRefined,
     response: QueryResponse,
