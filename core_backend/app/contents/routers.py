d8bd548aff46e31f1d5fecb7ddeceea0ecdcfbfd
amir_emami <amirali1376@gmail.com>
2024-06-22 15:20:10 +0100

    Add inital csv-upload endpoint (no tag management)

--- core_backend/app/contents/routers.py
+++ core_backend/app/contents/routers.py
@@ -1,7 +1,9 @@
 from typing import Annotated, List
 
-from fastapi import APIRouter, Depends
+import pandas as pd
+from fastapi import APIRouter, Depends, UploadFile
 from fastapi.exceptions import HTTPException
+from pandas.errors import EmptyDataError, ParserError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..auth.dependencies import get_current_user
@@ -156,6 +158,127 @@ async def retrieve_content_by_id(
         )
 
     return _convert_record_to_schema(record)
+
+
+@router.post("/csv-upload", response_model=dict)
+async def upload_contents_in_bulk(
+    file: UploadFile,
+    user_db: Annotated[UserDB, Depends(get_current_user)],
+    asession: AsyncSession = Depends(get_async_session),
+) -> dict:
+    """
+    Upload and process contents in bulk from a CSV file.
+    """
+
+    # TODO: deal with tags!
+
+    # Ensure the file is a CSV
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(
+            status_code=400, detail="Invalid file format. Please upload a CSV file."
+        )
+
+    df = _load_csv(file)
+    await _csv_checks(df=df, user_id=user_db.user_id, asession=asession)
+
+    # Add each row to the content database
+    for _, row in df.iterrows():
+        content = ContentCreate(
+            content_title=row["content_title"],
+            content_text=row["content_text"],
+            content_language=row.get("content_language", "ENGLISH"),
+            content_tags=[],
+            content_metadata=row.get("content_metadata", {}),
+        )
+        await save_content_to_db(
+            user_id=user_db.user_id, content=content, asession=asession
+        )
+
+    return {"message": f"Added {len(df)} new contents."}
+
+
+def _load_csv(file: UploadFile) -> pd.DataFrame:
+    """
+    Load the CSV file into a pandas DataFrame
+    """
+
+    try:
+        df = pd.read_csv(file.file)
+        return df
+    except EmptyDataError as e:
+        raise HTTPException(status_code=400, detail="The CSV file is empty.") from e
+    except ParserError as e:
+        raise HTTPException(
+            status_code=400, detail="CSV is unreadable (parsing error)"
+        ) from e
+    except UnicodeDecodeError as e:
+        raise HTTPException(
+            status_code=400, detail="CSV is unreadable (encoding error)"
+        ) from e
+
+
+async def _csv_checks(df: pd.DataFrame, user_id: int, asession: AsyncSession) -> None:
+    """
+    Perform checks on the CSV file to ensure it meets the requirements
+    """
+
+    # check if content_title and content_text columns are present
+    cols = df.columns
+    if "content_title" not in cols or "content_text" not in cols:
+        raise HTTPException(
+            status_code=400,
+            detail="File must have 'content_title' and 'content_text' columns.",
+        )
+    # check if there are any empty values in either column
+    if df["content_title"].isnull().any():
+        raise HTTPException(
+            status_code=400,
+            detail="One or more empty content titles found in the CSV file.",
+        )
+    if df["content_text"].isnull().any():
+        raise HTTPException(
+            status_code=400,
+            detail="One or more empty content texts found in the CSV file.",
+        )
+    # check if any title exceeds 150 characters
+    if df["content_title"].str.len().max() > 150:
+        raise HTTPException(
+            status_code=400, detail="One or more content titles exceed 150 characters."
+        )
+    # check if any text exceeds 2000 characters
+    if df["content_text"].str.len().max() > 2000:
+        raise HTTPException(
+            status_code=400, detail="One or more content texts exceed 2000 characters."
+        )
+
+    # check if there are duplicates in either column
+    if df.duplicated(subset=["content_title"]).any():
+        raise HTTPException(
+            status_code=400,
+            detail="Duplicate content titles found in the CSV file.",
+        )
+    if df.duplicated(subset=["content_text"]).any():
+        raise HTTPException(
+            status_code=400,
+            detail="Duplicate content text found in the CSV file.",
+        )
+
+    # check for duplicate titles and texts between the CSV and the database
+    contents_in_db = await get_list_of_content_from_db(
+        user_id, offset=0, limit=None, asession=asession
+    )
+    content_titles_in_db = [c.content_title for c in contents_in_db]
+    content_texts_in_db = [c.content_text for c in contents_in_db]
+    if df["content_title"].isin(content_titles_in_db).any():
+        raise HTTPException(
+            status_code=400,
+            detail="One or more content titles already exist in the database.",
+        )
+    if df["content_text"].isin(content_texts_in_db).any():
+        raise HTTPException(
+            status_code=400,
+            detail="One or more content texts already exist in the database.",
+        )
 
 
 def _convert_record_to_schema(record: ContentDB) -> ContentRetrieve:
