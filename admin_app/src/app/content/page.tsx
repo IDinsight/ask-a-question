635c142995e50c332523c619dc05bd2e693e6139
amir_emami <amirali1376@gmail.com>
2024-06-23 04:32:56 +0100

    Fix typing errors so npm run build runs

--- admin_app/src/app/content/page.tsx
+++ admin_app/src/app/content/page.tsx
@@ -29,7 +29,6 @@ import React, { MouseEvent, useState } from "react";
 import { ImportModal } from "../../components/ImportModal";
 import { PageNavigation } from "../../components/PageNavigation";
 import { SearchBar } from "../../components/SearchBar";
-import { ImportModal } from "../../components/ImportModal";
 
 const MAX_CARDS_TO_FETCH = 200;
 const MAX_CARDS_PER_PAGE = 12;
