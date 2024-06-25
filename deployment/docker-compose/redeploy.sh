b423e217d3f6e7cc0a1bd57d70b4a54a70a74a3e
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-06-25 22:58:18 +0530

    remove redeploy script

--- deployment/docker-compose/redeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/suzinyou/aaq-core
-git pull origin main
-cd ./deployment/docker-compose
-docker compose -f docker-compose.yml -f docker-compose.dev.yml -p aaq-stack up --build -d
-docker system prune --volumes -f
