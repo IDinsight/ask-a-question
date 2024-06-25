a754a1d811aca156cc59327195f664e2366a2b05
amir_emami <amirali1376@gmail.com>
2024-06-25 01:46:48 +0100

    Update integrations page skeleton

--- admin_app/src/app/integrations/page.tsx
+++ admin_app/src/app/integrations/page.tsx
@@ -8,8 +8,13 @@ import { sizes } from "@/utils";
 import { apiCalls } from "@/utils/api";
 import { useAuth } from "@/utils/auth";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
-import { Button, Typography } from "@mui/material";
+import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
 import React, { useState } from "react";
+
+interface CardImageProps {
+  src: string;
+  alt: string;
+}
 
 const IntegrationsPage = () => {
   const [currAccessLevel, setCurrAccessLevel] = React.useState("readonly");
@@ -20,12 +25,14 @@ const IntegrationsPage = () => {
   }, [accessLevel]);
 
   return (
-    <Layout.FlexBox alignItems="center" gap={sizes.baseGap}>
-      <Layout.Spacer multiplier={3} />
+    <Layout.FlexBox alignItems="center">
+      <Layout.Spacer multiplier={5} />
       <KeyManagement
         token={token}
         editAccess={currAccessLevel === "fullaccess"}
       />
+      <Layout.Spacer multiplier={4} />
+      <ChatManagersSection />
     </Layout.FlexBox>
   );
 };
@@ -37,7 +44,7 @@ const KeyManagement = ({
   token: string | null;
   editAccess: boolean;
 }) => {
-  const [currentKey, setCurrentKey] = useState("qwerty...");
+  // const [currentKey, setCurrentKey] = useState("qwerty...");
   const [confirmationModalOpen, setConfirmationModalOpen] = useState(false);
   const [newKeyModalOpen, setNewKeyModalOpen] = useState(false);
   const [newKey, setNewKey] = useState("");
@@ -74,35 +81,32 @@ const KeyManagement = ({
   return (
     <Layout.FlexBox
       key={"key-management"}
-      flexDirection={"column"}
-      sx={{
-        display: "flex",
-        alignSelf: "center",
-        px: sizes.baseGap,
-      }}
-      gap={sizes.baseGap}
+      flexDirection="column"
+      sx={{ maxWidth: 690, marginInline: 10 }}
+      gap={sizes.doubleBaseGap}
     >
-      <Typography variant="h5" align="center" color="primary">
-        API Key Management
+      <Typography variant="h5" color="primary" style={{ fontWeight: "bold" }}>
+        Your API Key
       </Typography>
       <Layout.FlexBox
-        flexDirection={"row"}
-        sx={{
-          display: "flex",
-          alignSelf: "center",
-          px: sizes.baseGap,
-        }}
-        gap={sizes.baseGap}
+        flexDirection="column"
+        // alignItems="center"
+        justifyContent="space-between"
+        gap={sizes.doubleBaseGap}
       >
+        <Typography variant="body1">
+          You will need your API key to interact with AAQ from your chat
+          manager. You can generate or re-generate your key here.
+        </Typography>
         <Button
           variant="contained"
           onClick={handleConfirmationModalOpen}
           disabled={!editAccess}
           startIcon={<AutorenewIcon />}
+          style={{ width: "220px", alignSelf: "center" }}
         >
           Generate New Key
         </Button>
-
         <KeyRenewConfirmationModal
           open={confirmationModalOpen}
           onClose={handleConfirmationModalClose}
@@ -120,12 +124,49 @@ const KeyManagement = ({
   );
 };
 
-// const IntegrationsGrid = ({
-//   token,
-//   accessLevel,
-// }: {
-//   token: string | null;
-//   accessLevel: string;
-// }) => { }
+const ChatManagersSection = () => {
+  return (
+    <Layout.FlexBox
+      key={"chat-managers"}
+      flexDirection="column"
+      sx={{ maxWidth: 1000, marginInline: 10 }}
+      gap={sizes.doubleBaseGap}
+    >
+      <Typography variant="h5" color="primary" style={{ fontWeight: "bold" }}>
+        Chat Managers
+      </Typography>
+      <Typography variant="body1">
+        Click on the chat manager of your choice to see instructions on how to
+        connect it to AAQ.
+      </Typography>
+      <ChatManagersGrid />
+    </Layout.FlexBox>
+  );
+};
+
+const ChatManagersGrid = () => {
+  const gridItems = [
+    { src: "https://path-to-typebot-logo.png", alt: "typebot" },
+    { src: "https://path-to-turn-logo.png", alt: "turn" },
+    { src: "https://path-to-glific-logo.png", alt: "Glific" },
+  ];
+  return (
+    <Grid container spacing={sizes.baseGap} style={{ minWidth: 220 }}>
+      {gridItems.map((item, index) => (
+        <Grid item xs={12} md={6} key={index}>
+          <CardImageComponent src={item.src} alt={item.alt} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+const CardImageComponent: React.FC<CardImageProps> = ({ src, alt }) => (
+  <Card>
+    <CardContent>
+      <img src={src} alt={alt} style={{ width: "100%" }} />
+    </CardContent>
+  </Card>
+);
 
 export default IntegrationsPage;
