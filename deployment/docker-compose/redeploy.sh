8fde17a341fc8eecf68a6253cb8e5ca582430676
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-06-25 13:26:58 +0530

    update OIDC provider and add prune statement to redeploy script

--- deployment/docker-compose/redeploy.sh
+++ deployment/docker-compose/redeploy.sh
@@ -4,3 +4,4 @@ cd /home/suzinyou/aaq-core
 git pull origin main
 cd ./deployment/docker-compose
 docker compose -f docker-compose.yml -f docker-compose.dev.yml -p aaq-stack up --build -d
+docker system prune --volumes -f
