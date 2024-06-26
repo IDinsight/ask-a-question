5e7721708be654cb38a3aad86b90f907d5c76ef1
Sid Ravinutala <sid.ravi1@gmail.com>
2024-06-26 12:50:43 -0400

    added stat cards backend

---
+++ core_backend/app/dashboard/schemas.py
@@ -0,0 +1,60 @@
+from pydantic import BaseModel
+
+
+class QueryStats(BaseModel):
+    """
+    This class is used to define the schema for the query stats
+    """
+
+    n_questions: int
+    percentage_increase: float
+
+
+class ResponseFeedbackStats(BaseModel):
+    """
+    This class is used to define the schema for the response feedback stats
+    """
+
+    n_positive: int
+    n_negative: int
+    percentage_positive_increase: float
+    percentage_negative_increase: float
+
+
+class ContentFeedbackStats(BaseModel):
+    """
+    This class is used to define the schema for the content feedback stats
+    """
+
+    n_positive: int
+    n_negative: int
+    percentage_positive_increase: float
+    percentage_negative_increase: float
+
+
+class UrgencyStats(BaseModel):
+    """
+    This class is used to define the schema for the urgency stats
+    """
+
+    n_urgent: int
+    percentage_increase: float
+
+
+class StatsCards(BaseModel):
+    """
+    This class is used to define the schema for the stats cards
+    """
+
+    query_stats: QueryStats
+    response_feedback_stats: ResponseFeedbackStats
+    content_feedback_stats: ContentFeedbackStats
+    urgency_stats: UrgencyStats
+
+
+class DashboardOverview(StatsCards):
+    """
+    This class is used to define the schema for the dashboard overview
+    """
+
+    stats_cards: StatsCards
