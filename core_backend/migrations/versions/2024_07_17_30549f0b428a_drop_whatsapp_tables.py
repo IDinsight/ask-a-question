31c242e49a20b085a52377793e86357f7e3727a1
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-07-17 20:33:37 +0530

    fix order of oprations due to foreign key constraint

--- core_backend/migrations/versions/2024_07_17_30549f0b428a_drop_whatsapp_tables.py
+++ core_backend/migrations/versions/2024_07_17_30549f0b428a_drop_whatsapp_tables.py
@@ -20,11 +20,29 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.drop_table("whatsapp_incoming_messages")
     op.drop_table("whatsapp_responses")
+    op.drop_table("whatsapp_incoming_messages")
 
 
 def downgrade() -> None:
+    op.create_table(
+        "whatsapp_incoming_messages",
+        sa.Column("incoming_id", sa.INTEGER(), autoincrement=True, nullable=False),
+        sa.Column(
+            "msg_id_from_whatsapp", sa.VARCHAR(), autoincrement=False, nullable=False
+        ),
+        sa.Column("phone_number", sa.VARCHAR(), autoincrement=False, nullable=False),
+        sa.Column("message", sa.VARCHAR(), autoincrement=False, nullable=False),
+        sa.Column("phonenumber_id", sa.VARCHAR(), autoincrement=False, nullable=False),
+        sa.Column(
+            "incoming_datetime_utc",
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint("incoming_id", name="whatsapp_incoming_messages_pkey"),
+    )
+
     op.create_table(
         "whatsapp_responses",
         sa.Column("response_id", sa.INTEGER(), autoincrement=True, nullable=False),
@@ -43,21 +61,4 @@ def downgrade() -> None:
             name="whatsapp_responses_incoming_id_fkey",
         ),
         sa.PrimaryKeyConstraint("response_id", name="whatsapp_responses_pkey"),
-    )
-    op.create_table(
-        "whatsapp_incoming_messages",
-        sa.Column("incoming_id", sa.INTEGER(), autoincrement=True, nullable=False),
-        sa.Column(
-            "msg_id_from_whatsapp", sa.VARCHAR(), autoincrement=False, nullable=False
-        ),
-        sa.Column("phone_number", sa.VARCHAR(), autoincrement=False, nullable=False),
-        sa.Column("message", sa.VARCHAR(), autoincrement=False, nullable=False),
-        sa.Column("phonenumber_id", sa.VARCHAR(), autoincrement=False, nullable=False),
-        sa.Column(
-            "incoming_datetime_utc",
-            postgresql.TIMESTAMP(),
-            autoincrement=False,
-            nullable=False,
-        ),
-        sa.PrimaryKeyConstraint("incoming_id", name="whatsapp_incoming_messages_pkey"),
     )
