4c05adbf4d8d0cb35d1ffa15012e1bf482d23cd3
Zhen Zhao <>
2024-07-18 12:36:36 -0400

    Hotfix for alembic migration issue due to previous index that was added by alembic but not part of ContentDB ORM definition.

---
+++ core_backend/tests/api/test_alembic_migrations.py
@@ -0,0 +1,63 @@
+"""This module tests alembic migrations using `pytest-alembic`.
+
+`pytest-alembic` defines two database fixtures, `alembic_config` and `alembic_engine`,
+which are used to test the migrations. These fixtures are defined in the local
+conftest.py.
+
+NB: The `alembic_runner` fixture is used to skip alembic tests.
+"""
+
+from typing import Any, Callable
+
+import alembic
+from pytest_alembic import create_alembic_fixture, tests
+
+migration_history = create_alembic_fixture()
+
+
+def test_single_head_revision(
+    alembic_runner: Callable[[dict[str, Any] | alembic.config.Config | None], Callable],
+    migration_history: tests.MigrationHistory,
+) -> None:
+    """Assert that there only exists one head revision."""
+
+    tests.test_single_head_revision(migration_history)
+
+
+def test_upgrade(
+    alembic_runner: Callable[[dict[str, Any] | alembic.config.Config | None], Callable],
+    migration_history: tests.MigrationHistory,
+) -> None:
+    """Assert that the revision history can be run through from base to head."""
+
+    tests.test_upgrade(migration_history)
+
+
+def test_model_definitions_match_ddl(
+    alembic_runner: Callable[[dict[str, Any] | alembic.config.Config | None], Callable],
+    migration_history: tests.MigrationHistory,
+) -> None:
+    """Assert that the state of the migrations matches the state of the models
+    describing the DDL.
+
+    In general, the set of migrations in the history should coalesce into DDL which is
+    described by the current set of models. Therefore, a call to revision --autogenerate
+    should always generate an empty migration (e.g. find no difference between your
+    database (i.e. migrations history) and your models).
+    """
+
+    tests.test_model_definitions_match_ddl(migration_history)
+
+
+def test_up_down_consistency(
+    alembic_runner: Callable[[dict[str, Any] | alembic.config.Config | None], Callable],
+    migration_history: tests.MigrationHistory,
+) -> None:
+    """Assert that all downgrades succeed.
+
+    While downgrading may not be lossless operation data-wise, there’s a theory of
+    database migrations that says that the revisions in existence for a database should
+    be able to go from an entirely blank schema to the finished product, and back again.
+    """
+
+    tests.test_up_down_consistency(migration_history)
