cf9eb36fc5f5c502e581340181c82952473909bd
amir_emami <amirali1376@gmail.com>
2024-07-19 18:37:52 +0100

    update migration file to avoid multiple heads issue

--- core_backend/migrations/versions/2024_07_17_f562cf512c90_rename_failed_rule_to_matched_rule_for_.py
+++ core_backend/migrations/versions/2024_07_17_f562cf512c90_rename_failed_rule_to_matched_rule_for_.py
@@ -1,7 +1,7 @@
 """rename failed_rule to matched_rule for UD
 
 Revision ID: f562cf512c90
-Revises: 4d3f01bf891f
+Revises: 30549f0b428a
 Create Date: 2024-07-17 13:58:29.227622
 
 """
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "f562cf512c90"
-down_revision: Union[str, None] = "4d3f01bf891f"
+down_revision: Union[str, None] = "30549f0b428a"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
