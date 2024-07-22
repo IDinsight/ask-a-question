698f0747f36ce92adea4adbc4eff5a05b9b3268d
Zhen Zhao <>
2024-07-22 15:49:09 -0400

    Updated to handle archive instead of deletion.

--- admin_app/src/components/ContentModal.tsx
+++ admin_app/src/components/ContentModal.tsx
@@ -1,5 +1,6 @@
 import { appColors, sizes } from "@/utils";
-import { Close, Delete, Edit, ThumbDown, ThumbUp } from "@mui/icons-material";
+// import { Close, Delete, Edit, ThumbDown, ThumbUp } from "@mui/icons-material";
+import { Archive, Close, Edit, ThumbDown, ThumbUp } from "@mui/icons-material";
 import {
   Box,
   Button,
@@ -187,20 +188,20 @@ const ContentViewModal = ({
   );
 };
 
-const DeleteContentModal = ({
+const ArchiveContentModal = ({
   content_id,
   open,
   onClose,
-  onSuccessfulDelete,
-  onFailedDelete,
-  deleteContent,
+  onSuccessfulArchive,
+  onFailedArchive,
+  archiveContent,
 }: {
   content_id: number;
   open: boolean;
   onClose: () => void;
-  onSuccessfulDelete: (content_id: number) => void;
-  onFailedDelete: (content_id: number) => void;
-  deleteContent: (content_id: number) => Promise<any>;
+  onSuccessfulArchive: (content_id: number) => void;
+  onFailedArchive: (content_id: number) => void;
+  archiveContent: (content_id: number) => Promise<any>;
 }) => {
   return (
     <Dialog
@@ -210,11 +211,11 @@ const DeleteContentModal = ({
       aria-describedby="alert-dialog-description"
     >
       <DialogTitle id="alert-dialog-title">
-        Are you sure you want to delete this content?
+        Are you sure you want to archive this content?
       </DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          Deleting this content will remove it from the database. This action
+          Archiving this content will remove it from the database. This action
           cannot be undone.
         </DialogContentText>
       </DialogContent>
@@ -222,29 +223,90 @@ const DeleteContentModal = ({
         <Button onClick={onClose}>Cancel</Button>
         <Button
           onClick={() => {
-            const handleDeleteContent = async (content_id: number) => {
-              const results = deleteContent(content_id)
+            const handleArchiveContent = async (content_id: number) => {
+              const results = archiveContent(content_id)
                 .then((res) => {
-                  onSuccessfulDelete(content_id);
+                  onSuccessfulArchive(content_id);
                 })
                 .catch((err) => {
                   console.log("error", err);
-                  onFailedDelete(content_id);
+                  onFailedArchive(content_id);
                 });
             };
-            handleDeleteContent(Number(content_id));
+            handleArchiveContent(Number(content_id));
             onClose();
           }}
           autoFocus
           variant="contained"
           color="error"
-          startIcon={<Delete />}
+          startIcon={<Archive />}
         >
-          Delete
+          Archive
         </Button>
       </DialogActions>
     </Dialog>
   );
 };
 
-export { ContentViewModal, DeleteContentModal };
+// const DeleteContentModal = ({
+//   content_id,
+//   open,
+//   onClose,
+//   onSuccessfulDelete,
+//   onFailedDelete,
+//   deleteContent,
+// }: {
+//   content_id: number;
+//   open: boolean;
+//   onClose: () => void;
+//   onSuccessfulDelete: (content_id: number) => void;
+//   onFailedDelete: (content_id: number) => void;
+//   deleteContent: (content_id: number) => Promise<any>;
+// }) => {
+//   return (
+//     <Dialog
+//       open={open}
+//       onClose={onClose}
+//       aria-labelledby="alert-dialog-title"
+//       aria-describedby="alert-dialog-description"
+//     >
+//       <DialogTitle id="alert-dialog-title">
+//         Are you sure you want to delete this content?
+//       </DialogTitle>
+//       <DialogContent>
+//         <DialogContentText id="alert-dialog-description">
+//           Deleting this content will remove it from the database. This action
+//           cannot be undone.
+//         </DialogContentText>
+//       </DialogContent>
+//       <DialogActions sx={{ marginBottom: 1, marginRight: 1 }}>
+//         <Button onClick={onClose}>Cancel</Button>
+//         <Button
+//           onClick={() => {
+//             const handleDeleteContent = async (content_id: number) => {
+//               const results = deleteContent(content_id)
+//                 .then((res) => {
+//                   onSuccessfulDelete(content_id);
+//                 })
+//                 .catch((err) => {
+//                   console.log("error", err);
+//                   onFailedDelete(content_id);
+//                 });
+//             };
+//             handleDeleteContent(Number(content_id));
+//             onClose();
+//           }}
+//           autoFocus
+//           variant="contained"
+//           color="error"
+//           startIcon={<Delete />}
+//         >
+//           Delete
+//         </Button>
+//       </DialogActions>
+//     </Dialog>
+//   );
+// };
+
+// export { ContentViewModal, DeleteContentModal };
+export { ContentViewModal, ArchiveContentModal };
