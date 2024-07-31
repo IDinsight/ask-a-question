7962ede230424b0c73f8b657bc76c393d9f59d3c
tonyzhao6 <>
2024-07-31 11:04:36 -0400

    Added docstrings. Tightened up logic.

--- core_backend/app/urgency_detection/models.py
+++ core_backend/app/urgency_detection/models.py
@@ -1,3 +1,7 @@
+"""This module contains the ORM for managing urgency detection queries in the
+`UrgencyQueryDB` database and functions for interacting with the database.
+"""
+
 from datetime import datetime, timezone
 from typing import List
 
@@ -12,8 +16,10 @@ from .schemas import UrgencyQuery, UrgencyResponse
 
 
 class UrgencyQueryDB(Base):
-    """
-    Urgency query database model.
+    """ORM for managing urgency detection queries.
+
+    This database ties into the Admin app and allows the user to view, add, edit,
+    and delete content in the `urgency-query` table.
     """
 
     __tablename__ = "urgency-query"
@@ -34,6 +40,20 @@ class UrgencyQueryDB(Base):
         "UrgencyResponseDB", back_populates="query", uselist=False, lazy=True
     )
 
+    def __repr__(self) -> str:
+        """Construct the string representation of the `UrgencyQueryDB` object.
+
+        Returns
+        -------
+        str
+            A string representation of the `UrgencyQueryDB` object.
+        """
+
+        return (
+            f"Urgency Query {self.urgency_query_id} for user #{self.user_id}\n"
+            f"message_text={self.message_text}"
+        )
+
 
 async def save_urgency_query_to_db(
     user_id: int,
@@ -41,9 +61,25 @@ async def save_urgency_query_to_db(
     urgency_query: UrgencyQuery,
     asession: AsyncSession,
 ) -> UrgencyQueryDB:
+    """Saves a user query to the database.
+
+    Parameters
+    ----------
+    user_id
+        The ID of the user requesting to save the urgency query to the database.
+    feedback_secret_key
+        The secret key for the feedback.
+    urgency_query
+        The urgency query to save to the database.
+    asession
+        `AsyncSession` object for database transactions.
+
+    Returns
+    -------
+    UrgencyQueryDB
+        The urgency query object that was saved to the database.
     """
-    Saves a user query to the database.
-    """
+
     urgency_query_db = UrgencyQueryDB(
         user_id=user_id,
         feedback_secret_key=feedback_secret_key,
@@ -59,23 +95,36 @@ async def save_urgency_query_to_db(
 async def check_secret_key_match(
     secret_key: str, query_id: int, asession: AsyncSession
 ) -> bool:
+    """Check if the secret key matches the one generated for the query ID.
+
+    Parameters
+    ----------
+    secret_key
+        The key to check.
+    query_id
+        The ID of the query to check.
+    asession
+        `AsyncSession` object for database transactions.
+
+    Returns
+    -------
+    bool
+        `True` if the secret key matches the one generated for the query ID, `False`
+        otherwise.
     """
-    Check if the secret key matches the one generated for query id
-    """
+
     stmt = select(UrgencyQueryDB.feedback_secret_key).where(
         UrgencyQueryDB.urgency_query_id == query_id
     )
     query_record = (await asession.execute(stmt)).first()
-
-    if (query_record is not None) and (query_record[0] == secret_key):
-        return True
-    else:
-        return False
+    return (query_record is not None) and (query_record[0] == secret_key)
 
 
 class UrgencyResponseDB(Base):
-    """
-    Urgency response database model.
+    """ORM for managing urgency responses.
+
+    This database ties into the Admin app and allows the user to view, add, edit,
+    and delete content in the `urgency-response` table.
     """
 
     __tablename__ = "urgency-response"
@@ -98,7 +147,14 @@ class UrgencyResponseDB(Base):
     )
 
     def __repr__(self) -> str:
-        """Pretty print the response."""
+        """Construct the string representation of the `UrgencyResponseDB` object.
+
+        Returns
+        -------
+        str
+            A string representation of the `UrgencyResponseDB` object.
+        """
+
         return (
             f"Urgency Response {self.urgency_response_id} for query #{self.query_id} "
             f"is_urgent={self.is_urgent}"
@@ -110,9 +166,23 @@ async def save_urgency_response_to_db(
     response: UrgencyResponse,
     asession: AsyncSession,
 ) -> UrgencyResponseDB:
+    """Saves the user query response to the database.
+
+    Parameters
+    ----------
+    urgency_query_db
+        The urgency query database object.
+    response
+        The urgency response object to save to the database.
+    asession
+        `AsyncSession` object for database transactions.
+
+    Returns
+    -------
+    UrgencyResponseDB
+        The urgency response object that was saved to the database.
     """
-    Saves the user query response to the database.
-    """
+
     urgency_query_responses_db = UrgencyResponseDB(
         query_id=urgency_query_db.urgency_query_id,
         is_urgent=response.is_urgent,
