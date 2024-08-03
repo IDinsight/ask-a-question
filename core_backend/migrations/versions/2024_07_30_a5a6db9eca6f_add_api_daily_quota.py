3c10d5ae743d1fec715e3493aa910098da4aba42
Sid Ravinutala <sid.ravi1@gmail.com>
2024-08-03 09:11:51 -0400

    redis version changed

--- core_backend/migrations/versions/2024_07_30_a5a6db9eca6f_add_api_daily_quota.py
+++ core_backend/migrations/versions/2024_07_30_a5a6db9eca6f_add_api_daily_quota.py
@@ -8,9 +8,8 @@ Create Date: 2024-07-04 13:31:11.544070
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "a5a6db9eca6f"
