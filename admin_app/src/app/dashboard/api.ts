5a38da85809a2a5586132e95d1aaafefa50a6822
Sid Ravinutala <sid.ravi1@gmail.com>
2024-08-05 09:24:32 -0400

    Dashboard: Page 2 -  line chart (#347)

--- admin_app/src/app/dashboard/api.ts
+++ admin_app/src/app/dashboard/api.ts
@@ -16,9 +16,33 @@ const getOverviewPageData = async (period: Period, token: string) => {
       let resp = response.json();
       return resp;
     } else {
-      throw new Error("Error fetching dashboard stats card data");
+      throw new Error("Error fetching dashboard overview page data");
     }
   });
 };
 
-export { getOverviewPageData };
+const getPerformancePageData = async (
+  period: Period,
+  token: string,
+  top_n: number,
+) => {
+  return fetch(
+    `${NEXT_PUBLIC_BACKEND_URL}/dashboard/performance/${period}?top_n=${top_n}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  ).then((response) => {
+    if (response.ok) {
+      let resp = response.json();
+      return resp;
+    } else {
+      throw new Error("Error fetching dashboard performance page data");
+    }
+  });
+};
+
+export { getOverviewPageData, getPerformancePageData };
