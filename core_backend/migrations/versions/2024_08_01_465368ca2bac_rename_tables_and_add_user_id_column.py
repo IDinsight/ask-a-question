7e62931b387f80c97d936b603cfec96414e48ce6
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-08-06 11:43:39 +0530

    Update core_backend/migrations/versions/2024_08_01_465368ca2bac_rename_tables_and_add_user_id_column.py
    Co-authored-by: Sid Ravinutala <sid.ravi1@gmail.com>

--- core_backend/migrations/versions/2024_08_01_465368ca2bac_rename_tables_and_add_user_id_column.py
+++ core_backend/migrations/versions/2024_08_01_465368ca2bac_rename_tables_and_add_user_id_column.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "465368ca2bac"  # pragma: allowlist secret
-down_revision: Union[str, None] = "9d0519332fdd"
+down_revision: Union[str, None] = "b87b336df479"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
