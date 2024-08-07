9fa2aa4517af2a0ed66b9c5382e59fd018f9f458
MustafaAkolawala <mustafaakolawala@eng.rizvi.edu.in>
2024-08-07 21:10:23 +0530

    Solve Multiple heads conflict

--- core_backend/migrations/versions/2024_08_07_7192d62a0b83_add_generate_tts_column_to_query_table_.py
+++ core_backend/migrations/versions/2024_08_07_7192d62a0b83_add_generate_tts_column_to_query_table_.py
@@ -1,8 +1,8 @@
 """add generate_tts column to query table and tts_file column to query response table
 
 Revision ID: 7192d62a0b83
-Revises: b87b336df479
-Create Date: 2024-08-06 18:50:40.834060
+Revises: 465368ca2bac
+Create Date: 2024-08-07 18:50:40.834060
 
 """
 
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "7192d62a0b83"  # pragma: allowlist secret
-down_revision: Union[str, None] = "b87b336df479"
+down_revision: Union[str, None] = "465368ca2bac"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
