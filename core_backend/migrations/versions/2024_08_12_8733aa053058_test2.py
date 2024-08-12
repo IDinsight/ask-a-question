c2475d085ccbed80e920d6315d005f3ec20b6233
tonyzhao6 <>
2024-08-12 10:48:35 -0400

    Temp changes.

--- core_backend/migrations/versions/2024_08_12_8733aa053058_test2.py
+++ core_backend/migrations/versions/2024_08_12_8733aa053058_test2.py
@@ -1,8 +1,8 @@
 """test2
 
-Revision ID: d567aa0f1264
-Revises: 465368ca2bac
-Create Date: 2024-08-12 10:43:41.331710
+Revision ID: 8733aa053058
+Revises: 358588881e01
+Create Date: 2024-08-12 10:48:01.003542
 
 """
 from typing import Sequence, Union
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'd567aa0f1264'
-down_revision: Union[str, None] = '465368ca2bac'
+revision: str = '8733aa053058'
+down_revision: Union[str, None] = '358588881e01'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
