96ea2e1f666ab3979739750d25dde0675c678876
tonyzhao6 <>
2024-08-12 09:48:06 -0400

    Temp changes.

---
+++ core_backend/migrations/versions/2024_08_12_46b225b88c93_test1.py
@@ -0,0 +1,23 @@
+"""test1
+
+Revision ID: 46b225b88c93
+Revises: 358588881e01
+Create Date: 2024-08-12 09:47:42.312118
+
+"""
+
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = "46b225b88c93"
+down_revision: Union[str, None] = "358588881e01"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
