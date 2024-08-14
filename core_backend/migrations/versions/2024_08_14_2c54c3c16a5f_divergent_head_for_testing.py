4fd0e4cb291b5003a46ee53cb248f5676d849977
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-08-14 17:34:56 +0530

    add divergent head from tony's main

---
+++ core_backend/migrations/versions/2024_08_14_2c54c3c16a5f_divergent_head_for_testing.py
@@ -0,0 +1,23 @@
+"""DIVERGENT_HEAD_FOR_TESTING
+
+Revision ID: 2c54c3c16a5f
+Revises: 7192d62a0b83
+Create Date: 2024-08-14 17:33:40.270320
+
+"""
+
+from typing import Sequence, Union
+
+# revision identifiers, used by Alembic.
+revision: str = "2c54c3c16a5f"  # pragma: allowlist secret
+down_revision: Union[str, None] = "7192d62a0b83"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
