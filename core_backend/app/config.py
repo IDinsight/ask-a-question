37593f6adc2dbc5126dc6374cc75919f36ce2f48
Suzin You <7042047+suzinyou@users.noreply.github.com>
2024-08-26 10:44:13 +0530

    [AAQ-751] Temporary workaround for Emojis in content (#405)
    * temporary workaround of making vertex ai requests directly from backend
    
    * test litellm proxy fallback
    
    * undo direct vertex ai requests from backend

--- core_backend/app/config.py
+++ core_backend/app/config.py
@@ -36,15 +36,10 @@ LITELLM_API_KEY = os.environ.get("LITELLM_API_KEY", "dummy-key")
 LITELLM_MODEL_EMBEDDING = os.environ.get("LITELLM_MODEL_EMBEDDING", "openai/embeddings")
 LITELLM_MODEL_DEFAULT = os.environ.get("LITELLM_MODEL_DEFAULT", "openai/default")
 LITELLM_MODEL_GENERATION = os.environ.get(
-    "LITELLM_MODEL_GENERATION",
-    "openai/generate-gemini-response",
-    # "LITELLM_MODEL_GENERATION", "openai/generate-response"
+    "LITELLM_MODEL_GENERATION", "openai/generate-response"
 )
 LITELLM_MODEL_LANGUAGE_DETECT = os.environ.get(
     "LITELLM_MODEL_LANGUAGE_DETECT", "openai/detect-language"
-)
-LITELLM_MODEL_ON_OFF_TOPIC = os.environ.get(
-    "LITELLM_MODEL_ON_OFF_TOPIC", "openai/on-off-topic"
 )
 LITELLM_MODEL_TRANSLATE = os.environ.get("LITELLM_MODEL_TRANSLATE", "openai/translate")
 LITELLM_MODEL_SAFETY = os.environ.get("LITELLM_MODEL_SAFETY", "openai/safety")
