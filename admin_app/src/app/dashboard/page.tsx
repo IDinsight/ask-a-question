4d10d2f3b865455fc72494476f3a20abbdcd9caa
amir_emami <amirali1376@gmail.com>
2024-08-28 12:23:37 +0530

    Add descriptions to dashboard pages

--- admin_app/src/app/dashboard/page.tsx
+++ admin_app/src/app/dashboard/page.tsx
@@ -1,34 +1,45 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import { Sidebar, PageName } from "@/app/dashboard/components/Sidebar";
 import TabPanel from "@/app/dashboard/components/TabPanel";
 import { Period, drawerWidth } from "./types";
 import Overview from "@/app/dashboard/components/Overview";
 import ContentPerformance from "@/app/dashboard/components/ContentPerformance";
-import { useState } from "react";
 import { appColors } from "@/utils";
 import { ClickAwayListener } from "@mui/base/ClickAwayListener";
 
+type Page = {
+  name: PageName;
+  description: string;
+};
+
+const pages: Page[] = [
+  { name: "Overview", description: "Overview of the dashboard" },
+  { name: "Content Performance", description: "Performance of your content" },
+  { name: "Content Gaps", description: "Identify gaps in your content" },
+];
+
 const Dashboard: React.FC = () => {
-  const [dashboardPage, setDashboardPage] = useState<PageName>("Overview");
+  const [dashboardPage, setDashboardPage] = useState<Page>(pages[0]);
   const [timePeriod, setTimePeriod] = useState<Period>("week" as Period);
   const [sideBarOpen, setSideBarOpen] = useState<boolean>(true);
+
   const handleTabChange = (_: React.ChangeEvent<{}>, newValue: Period) => {
     setTimePeriod(newValue);
   };
 
   const showPage = () => {
-    switch (dashboardPage) {
+    switch (dashboardPage.name) {
       case "Overview":
         return <Overview timePeriod={timePeriod} />;
       case "Content Performance":
         return <ContentPerformance timePeriod={timePeriod} />;
       case "Content Gaps":
-        return <div>Products</div>;
+        return <div>Coming Soon!</div>;
       default:
-        return <div>Page not found</div>;
+        return <div>Page not found.</div>;
     }
   };
 
@@ -65,8 +76,11 @@ const Dashboard: React.FC = () => {
         <Sidebar
           open={sideBarOpen}
           setOpen={setSideBarOpen}
-          setDashboardPage={setDashboardPage}
-          selectedDashboardPage={dashboardPage}
+          setDashboardPage={(pageName: PageName) => {
+            const page = pages.find((p) => p.name === pageName);
+            if (page) setDashboardPage(page);
+          }}
+          selectedDashboardPage={dashboardPage.name}
         />
       </ClickAwayListener>
       <Box
@@ -88,13 +102,19 @@ const Dashboard: React.FC = () => {
           <Box
             sx={{
               display: "flex",
-              py: 2,
+              flexDirection: "column",
+              paddingInline: 2,
+              paddingBottom: 1,
+              gap: 2,
               borderBottom: "1px solid",
               borderBottomColor: "divider",
             }}
           >
             <Typography variant="h4" color={appColors.primary}>
-              {dashboardPage}
+              {dashboardPage.name}
+            </Typography>
+            <Typography variant="body1" align="left">
+              {dashboardPage.description}
             </Typography>
           </Box>
           <TabPanel tabValue={timePeriod} handleChange={handleTabChange} />
