6e228ceee6f7695cbd44982f374fa26a9bd09a90
Mark Botterill <markbotters@gmail.com>
2024-10-01 17:16:19 +0300

    Add timeout logic so model can download if needed

--- core_backend/startup.sh
+++ core_backend/startup.sh
@@ -2,5 +2,5 @@
 python -m alembic upgrade head
 python add_users_to_db.py
 exec gunicorn -k main.Worker -w 4 -b 0.0.0.0:8000 --preload \
-    -c gunicorn_hooks_config.py main:app
+    -c gunicorn_hooks_config.py main:app --timeout 90
 #
