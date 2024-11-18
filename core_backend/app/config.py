02fcf42f6ceace71ef69e2d4071e5d3a563fba92
tonyzhao6 <>
2024-11-18 17:00:13 -0500

    Added REDIS timeout variable to config.

--- core_backend/app/config.py
+++ core_backend/app/config.py
@@ -95,6 +95,7 @@ DB_POOL_SIZE = os.environ.get("DB_POOL_SIZE", 20)  # Number of connections in th
 
 # Redis
 REDIS_HOST = os.environ.get("REDIS_HOST", "redis://localhost:6379")
+REDIS_CHAT_CACHE_EXPIRY_TIME = 3600
 
 # Google Cloud storage
 GCS_SPEECH_BUCKET = os.environ.get("GCS_SPEECH_BUCKET", "aaq-speech-test")
