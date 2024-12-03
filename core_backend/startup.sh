4b9e42079b884363ee3b506356231a95e21ae304
Mark Botterill <97025274+markbotterill@users.noreply.github.com>
2024-12-03 12:46:37 +0000

    [HOTFIX] Topic modeling failure (#462)
    * Add 100s timeout to Caddy
    
    * Add 100s timeout to gunicorn

--- core_backend/startup.sh
+++ core_backend/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python -m alembic upgrade head
 exec gunicorn -k main.Worker -w 4 -b 0.0.0.0:8000 --preload \
-    -c gunicorn_hooks_config.py main:app
+    -c gunicorn_hooks_config.py main:app -t 100
 #
