f6984a986eee04f0ac612820fc7e98a3ceffd665
tonyzhao6 <>
2025-02-07 07:50:26 -0500

    Returning WorkspaceRetrieve after creating workspaces instead of WorkspaceCreate so that workspace_id is available.

--- core_backend/app/workspaces/routers.py
+++ core_backend/app/workspaces/routers.py
@@ -49,12 +49,12 @@ router = APIRouter(prefix="/workspace", tags=["Workspace"])
 logger = setup_logger()
 
 
-@router.post("/", response_model=list[WorkspaceCreate])
+@router.post("/", response_model=list[WorkspaceRetrieve])
 async def create_workspaces(
     calling_user_db: Annotated[UserDB, Depends(get_current_user)],
     workspaces: WorkspaceCreate | list[WorkspaceCreate],
     asession: AsyncSession = Depends(get_async_session),
-) -> list[WorkspaceCreate]:
+) -> list[WorkspaceRetrieve]:
     """Create workspaces. Workspaces can only be created by ADMIN users.
 
     NB: Any user is allowed to create a workspace. However, the user must be assigned
@@ -88,7 +88,7 @@ async def create_workspaces(
 
     Returns
     -------
-    list[WorkspaceCreate]
+    list[WorkspaceRetrieve]
         A list of created workspace objects.
 
     Raises
@@ -108,7 +108,7 @@ async def create_workspaces(
 
     if not isinstance(workspaces, list):
         workspaces = [workspaces]
-    created_workspaces: list[WorkspaceCreate] = []
+    created_workspaces: list[WorkspaceRetrieve] = []
 
     for workspace in workspaces:
         # 1.
@@ -136,9 +136,14 @@ async def create_workspaces(
                 workspace_db=workspace_db,
             )
             created_workspaces.append(
-                WorkspaceCreate(
+                WorkspaceRetrieve(
                     api_daily_quota=workspace_db.api_daily_quota,
+                    api_key_first_characters=workspace_db.api_key_first_characters,
+                    api_key_updated_datetime_utc=workspace_db.api_key_updated_datetime_utc,
                     content_quota=workspace_db.content_quota,
+                    created_datetime_utc=workspace_db.created_datetime_utc,
+                    updated_datetime_utc=workspace_db.updated_datetime_utc,
+                    workspace_id=workspace_db.workspace_id,
                     workspace_name=workspace_db.workspace_name,
                 )
             )
